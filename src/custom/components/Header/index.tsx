b303facd9e0559062b304bba798a3f6b62dcf8ca
Anxo Rodriguez <anxolin@gmail.com>
2020-10-28 13:16:34 +0100

    Leave header placeholder

--- src/custom/components/Header/index.tsx
+++ src/custom/components/Header/index.tsx
@@ -1,11 +1,8 @@
 import HeaderMod from './HeaderMod'
 import styled from 'styled-components'
 
-const Header = styled(HeaderMod)`
-  color: red;
-  font-size: 500%;
-  padding: 5em;
-  background-color: red !important;
+export const Header = styled(HeaderMod)`
+  // CSS Override
 `
 
 export default Header
