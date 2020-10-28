a279777fe52a3d4c738d495b0415bc5081a76071
Anxo Rodriguez <anxolin@gmail.com>
2020-10-28 12:37:29 +0100

    Add modified header

---
+++ src/custom/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import HeaderMod from './HeaderMod'
+import styled from 'styled-components'
+
+const Header = styled(HeaderMod)`
+  color: red;
+  font-size: 500%;
+  padding: 5em;
+  background-color: red !important;
+`
+
+export default Header
