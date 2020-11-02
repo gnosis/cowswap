2ceb9e9238be44bba67e40aab7702a97a305b6b9
Velenir <velenir@gmail.com>
2020-11-02 17:46:20 +0300

    disable aws sync

--- .github/scripts/deploy_pull_request.sh
+++ .github/scripts/deploy_pull_request.sh
@@ -17,7 +17,8 @@ function deploy_pull_request {
   REVIEW_FEATURE_FOLDER="$REPO_NAME_ALPHANUMERIC/$PULL_REQUEST_NAME"
 
   # Deploy project
-  aws s3 sync dist s3://${REVIEW_BUCKET_NAME}/${REVIEW_FEATURE_FOLDER} --delete
+  #Disabled for now
+  #aws s3 sync dist s3://${REVIEW_BUCKET_NAME}/${REVIEW_FEATURE_FOLDER} --delete
 }
 
 function publish_pull_request_urls_in_github {
