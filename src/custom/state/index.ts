eda6633e4c033b019fbd2f99c66046b9b6a5b3fc
Velenir <velenir@gmail.com>
2020-11-24 17:57:07 +0300

    copy state/index to custom

---
+++ src/custom/state/index.ts
@@ -0,0 +1,36 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { save, load } from 'redux-localstorage-simple'
+
+import application from '@src/state/application/reducer'
+import { updateVersion } from '@src/state/global/actions'
+import user from '@src/state/user/reducer'
+import transactions from '@src/state/transactions/reducer'
+import swap from '@src/state/swap/reducer'
+import mint from '@src/state/mint/reducer'
+import lists from '@src/state/lists/reducer'
+import burn from '@src/state/burn/reducer'
+import multicall from '@src/state/multicall/reducer'
+
+const PERSISTED_KEYS: string[] = ['user', 'transactions', 'lists']
+
+const store = configureStore({
+  reducer: {
+    application,
+    user,
+    transactions,
+    swap,
+    mint,
+    burn,
+    multicall,
+    lists
+  },
+  middleware: [...getDefaultMiddleware({ thunk: false }), save({ states: PERSISTED_KEYS })],
+  preloadedState: load({ states: PERSISTED_KEYS })
+})
+
+store.dispatch(updateVersion())
+
+export default store
+
+export type AppState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
