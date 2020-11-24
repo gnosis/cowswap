18124db4a3ab7f85ca61cf207f4056e3fc03750a
Anxo Rodriguez <anxolin@gmail.com>
2020-11-24 13:44:31 +0100

    Provide mock implementation

--- src/custom/hooks/useSwapCallback.ts
+++ src/custom/hooks/useSwapCallback.ts
@@ -1,13 +1,56 @@
 import { SwapCallbackState } from '@src/hooks/useSwapCallback'
 import { INITIAL_ALLOWED_SLIPPAGE } from 'constants/index'
 
-import { useSwapCallback as useSwapCallbackUniswap } from '@src/hooks/useSwapCallback'
+// import { useSwapCallback as useSwapCallbackUniswap } from '@src/hooks/useSwapCallback'
 import { Trade } from '@uniswap/sdk'
+import { useActiveWeb3React } from '@src/hooks'
+import useENS from '@src/hooks/useENS'
+import { useMemo } from 'react'
 
+// returns a function that will execute a swap, if the parameters are all valid
+// and the user has approved the slippage adjusted input amount for the trade
 export function useSwapCallback(
   trade: Trade | undefined, // trade to execute, required
   allowedSlippage: number = INITIAL_ALLOWED_SLIPPAGE, // in bips
   recipientAddressOrName: string | null // the ENS name or address of the recipient of the trade, or null if swap should be returned to sender
 ): { state: SwapCallbackState; callback: null | (() => Promise<string>); error: string | null } {
-  return useSwapCallbackUniswap(trade, allowedSlippage, recipientAddressOrName)
+  const { account, chainId, library } = useActiveWeb3React()
+  const { address: recipientAddress } = useENS(recipientAddressOrName)
+  const recipient = recipientAddressOrName === null ? account : recipientAddress
+
+  return useMemo(() => {
+    if (!trade || !library || !account || !chainId) {
+      return { state: SwapCallbackState.INVALID, callback: null, error: 'Missing dependencies' }
+    }
+    if (!recipient) {
+      if (recipientAddressOrName !== null) {
+        return { state: SwapCallbackState.INVALID, callback: null, error: 'Invalid recipient' }
+      } else {
+        return { state: SwapCallbackState.LOADING, callback: null, error: null }
+      }
+    }
+
+    return {
+      state: SwapCallbackState.VALID,
+      callback: async function onSwap(): Promise<string> {
+        console.log(`[useSwapCallback] Trading for ${recipient}. Network ${chainId}. Data: `, {
+          trade,
+          allowedSlippage,
+          recipientAddressOrName
+        })
+        return new Promise(async (resolve, reject) => {
+          setTimeout(() => {
+            if (Math.random() % 3 === 0) {
+              console.log('[useSwapCallback] Ups, we had a small issue!')
+              reject(new Error('Mock error: The flux capacitor melted'))
+            } else {
+              console.log('[useSwapCallback] Traded successfully!')
+              resolve('0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c')
+            }
+          }, 3000)
+        })
+      },
+      error: null
+    }
+  }, [trade, library, account, chainId, recipient, allowedSlippage, recipientAddressOrName])
 }
