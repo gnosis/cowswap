aa899b9065621ba4391cbac3caf80df3c603894f
David Sato <david.sato64@gmail.com>
2020-11-27 12:34:38 +0100

    dont make test tip dynamic

--- src/custom/utils/price.ts
+++ src/custom/utils/price.ts
@@ -3,9 +3,4 @@ export * from '@src/utils/prices'
 const DEFAULT_TIP = '5'
 
 // TODO: add mock API using Alex's created open API definition of tip endpoint
-export const getTip = (): string | undefined => {
-  // TODO: replace with fetch call to OBA-API TIP endpoint
-  const fakeTip: string | undefined = !!(Math.random() % 3) ? DEFAULT_TIP : undefined
-
-  return fakeTip
-}
+export const getTip = (): string => DEFAULT_TIP
