17507c0790705af1257ce1c4ba3a25812eb91f1f
David Sato <david.sato64@gmail.com>
2020-12-03 12:19:47 +0100

    make default tip 0

--- src/custom/utils/price.ts
+++ src/custom/utils/price.ts
@@ -1,6 +1,6 @@
 export * from '@src/utils/prices'
 
-const DEFAULT_TIP = '5'
+const DEFAULT_TIP = '0'
 
 // TODO: add mock API using Alex's created open API definition of tip endpoint
 export const getTip = (): string => DEFAULT_TIP
