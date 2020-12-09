3ad448e40d74af09c197f69b242e6248312e3cf1
Anxo Rodriguez <anxolin@gmail.com>
2020-12-09 16:33:04 +0100

    Load allowance manager address using SDK

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -1,4 +1,5 @@
 import { ChainId } from '@uniswap/sdk'
+import { allowanceManagerAddress } from '@gnosis.pm/gp-v2-contracts'
 
 // reexport everything
 export * from '@src/constants/index'
@@ -10,8 +11,8 @@ export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = 
   // TODO: extend chainId for adding xDAI
 }
 
-export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
-  [ChainId.MAINNET]: '0x6F400810b62df8E13fded51bE75fF5393eaa841F',
-  [ChainId.RINKEBY]: '0xC576eA7bd102F7E476368a5E98FA455d1Ea34dE2'
+export const GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
+  [ChainId.MAINNET]: allowanceManagerAddress(GP_SETTLEMENT_CONTRACT_ADDRESS[ChainId.MAINNET] as string),
+  [ChainId.RINKEBY]: allowanceManagerAddress(GP_SETTLEMENT_CONTRACT_ADDRESS[ChainId.RINKEBY] as string)
   // TODO: extend chainId for adding xDAI
 }
