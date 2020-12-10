f561ead0ec3ce3574a84a9aebd5f1b7d848d393e
Velenir <Velenir@users.noreply.github.com>
2020-12-10 18:06:30 +0300

    fix name from suggested changes :(

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -10,7 +10,7 @@ export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = 
   // TODO: extend chainId for adding xDAI
 }
 
-export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
+export const GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: '0x6F400810b62df8E13fded51bE75fF5393eaa841F',
   [ChainId.RINKEBY]: '0xC576eA7bd102F7E476368a5E98FA455d1Ea34dE2'
   // TODO: extend chainId for adding xDAI
