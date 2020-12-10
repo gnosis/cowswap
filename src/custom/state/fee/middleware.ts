9c69957fb9ba2c0a141fd4f3686978ffbf892100
David Sato <david.sato64@gmail.com>
2020-12-10 14:15:36 +0100

    update fee on expiration

--- src/custom/state/fee/middleware.ts
+++ src/custom/state/fee/middleware.ts
@@ -2,33 +2,39 @@ import { isAnyOf, Middleware } from '@reduxjs/toolkit'
 import { getFee } from 'utils/fees'
 import { replaceSwapState, selectCurrency, switchCurrencies } from '@src/state/swap/actions'
 import { updateFee } from './actions'
-import { FeeInformationState } from './reducer'
-import { SwapState } from '@src/state/swap/reducer'
+import { AppState } from '..'
 
 const isCurrencyChangeAction = isAnyOf(selectCurrency, replaceSwapState, switchCurrencies)
 const isSelectCurrency = isAnyOf(selectCurrency)
 const isReplaceState = isAnyOf(replaceSwapState)
 
+function isDateLater(dateA: string, dateB: string): boolean {
+  const [parsedDateA, parsedDateB] = [Date.parse(dateA), Date.parse(dateB)]
+
+  return parsedDateA > parsedDateB
+}
+
 export const applyFeeMiddleware: Middleware = ({ dispatch, getState }) => next => async action => {
   if (isCurrencyChangeAction(action)) {
     const {
-      swap: {
-        OUTPUT: { currencyId }
-      },
+      swap: { OUTPUT },
       fee: { feesMap }
-    }: { fee: FeeInformationState; swap: SwapState } = getState()
+    }: Pick<AppState, 'fee' | 'swap'> = getState()
 
-    let currencyIdFromPayload: string | undefined = currencyId
+    let currencyIdFromPayload: string | undefined = OUTPUT.currencyId
 
+    // if action is currenct selection, use that payload structure
     if (isSelectCurrency(action)) currencyIdFromPayload = action.payload.currencyId
+    // payload is state replacement action
     else if (isReplaceState(action)) currencyIdFromPayload = action.payload.inputCurrencyId
 
     // we have a explicity token address (currencyIdFromPayload) within payload
     // use this to getFee
     if (currencyIdFromPayload) {
       const tokenFee = feesMap[currencyIdFromPayload]?.fee
+      const isFeeDateValid = tokenFee && isDateLater(tokenFee.expirationDate, new Date().toISOString())
 
-      if (!tokenFee) {
+      if (!isFeeDateValid || !tokenFee) {
         const fee = await getFee(currencyIdFromPayload)
         dispatch(
           updateFee({
