fd05355de7722520ae19a367ff5fdc6f2dc676a9
David Sato <david.sato64@gmail.com>
2020-12-10 16:47:25 +0100

    remove unused code, try/catch getFee

--- src/custom/utils/fees.ts
+++ src/custom/utils/fees.ts
@@ -1,15 +1,9 @@
-import { Percent, JSBI, Fraction } from '@uniswap/sdk'
+import { Percent, JSBI } from '@uniswap/sdk'
 import { FeeInformation } from '../state/fee/reducer'
 
 // TODO: remove
 const DEFAULT_MINIMAL_FEE = '10'
 const DEFAULT_BASE_FEE = new Percent(JSBI.BigInt(10), JSBI.BigInt(10000))
-export const ONE_HUNDRED_PERCENT = new Percent(JSBI.BigInt(10000), JSBI.BigInt(10000))
-
-type MathType = string | bigint | Fraction | Percent | JSBI
-
-export const computeFee = (percentFee: MathType, multiplicand: MathType) =>
-  ONE_HUNDRED_PERCENT.subtract(percentFee).multiply(multiplicand)
 
 const MOCK_FEE_INFORMATION: FeeInformation = {
   expirationDate: new Date().toISOString(),
@@ -22,14 +16,20 @@ const MOCK_FEE_INFORMATION: FeeInformation = {
  * @param currencyId sellToken address as string
  * @description Consume fee endpoint
  */
-export const getFee = async (currencyId: string): Promise<FeeInformation> => {
-  return new Promise(accept => {
-    return setTimeout(() => {
-      console.debug(
-        `[MOCK] utils/price::getFee ==> https://protocol.dev.gnosisdev.com/api/v1/fee/${currencyId}`,
-        MOCK_FEE_INFORMATION
-      )
-      accept(MOCK_FEE_INFORMATION)
-    }, Math.floor(Math.random() * 1000))
-  })
+export const getFee = async (currencyId: string): Promise<FeeInformation | void> => {
+  try {
+    const feeInformation = await new Promise<FeeInformation>(accept => {
+      return setTimeout(() => {
+        console.debug(
+          `[MOCK] utils/price::getFee ==> https://protocol.dev.gnosisdev.com/api/v1/fee/${currencyId}`,
+          MOCK_FEE_INFORMATION
+        )
+        accept(MOCK_FEE_INFORMATION)
+      }, Math.floor(Math.random() * 1000))
+    })
+
+    return feeInformation
+  } catch (error) {
+    console.error('[FEE INFORMATION] Error fetching fee:', error)
+  }
 }
