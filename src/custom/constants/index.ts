899a82fbe94147c48e36c02f489e9c860eda1d76
Anxo Rodriguez <anxolin@gmail.com>
2020-12-10 13:17:15 +0100

    Allow partial network definitions
    Co-authored-by: Velenir <Velenir@users.noreply.github.com>

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -4,20 +4,14 @@ import { ChainId } from '@uniswap/sdk'
 export * from '@src/constants/index'
 
 // TODO: When contracts are deployed, we can load this from the NPM package
-export const GP_SETTLEMENT_CONTRACT_ADDRESS: Record<ChainId, string | undefined> = {
+export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: '0x6F400810b62df8E13fded51bE75fF5393eaa841F',
-  [ChainId.RINKEBY]: '0xC576eA7bd102F7E476368a5E98FA455d1Ea34dE2',
-  [ChainId.GÖRLI]: undefined,
-  [ChainId.KOVAN]: undefined,
-  [ChainId.ROPSTEN]: undefined
+  [ChainId.RINKEBY]: '0xC576eA7bd102F7E476368a5E98FA455d1Ea34dE2'
   // TODO: extend chainId for adding xDAI
 }
 
-export const GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS: Record<ChainId, string | undefined> = {
+export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: '0x6F400810b62df8E13fded51bE75fF5393eaa841F',
-  [ChainId.RINKEBY]: '0xC576eA7bd102F7E476368a5E98FA455d1Ea34dE2',
-  [ChainId.GÖRLI]: undefined,
-  [ChainId.KOVAN]: undefined,
-  [ChainId.ROPSTEN]: undefined
+  [ChainId.RINKEBY]: '0xC576eA7bd102F7E476368a5E98FA455d1Ea34dE2'
   // TODO: extend chainId for adding xDAI
 }
