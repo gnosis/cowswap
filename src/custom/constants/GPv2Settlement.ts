0428b7ddd3c856529f929014093fd7a5624699d9
Velenir <velenir@gmail.com>
2020-12-14 11:57:54 +0300

    prune ABI

--- src/custom/constants/GPv2Settlement.ts
+++ src/custom/constants/GPv2Settlement.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '@uniswap/sdk'
 import { Interface } from '@ethersproject/abi'
-// Can even prune ABI down to onlywhat we use
-// like events only
+// lightweight ABI
+// contains only Trade event and allowanceManager method
 import GP_V2_SETTLEMENT from './GPv2Settlement.json'
 
 export const GP_V2_SETTLEMENT_INTERFACE = new Interface(GP_V2_SETTLEMENT)
