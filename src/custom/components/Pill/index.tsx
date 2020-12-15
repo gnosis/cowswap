a5e70a568c2a8214218270c73f71ec2169961cd0
David Sato <david.sato64@gmail.com>
2020-12-15 12:53:34 +0100

    colour prop to pill

--- src/custom/components/Pill/index.tsx
+++ src/custom/components/Pill/index.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
-const Pill = styled.strong<{ bgColor: string }>`
+const Pill = styled.strong<{ color?: string; bgColor?: string }>`
   padding: 0.2rem 0.4rem;
   border-radius: 1rem;
-  background-color: ${(props): string => props.bgColor};
+  color: ${(props): string => props.color || 'inherit'};
+  background-color: ${(props): string => props.bgColor || 'transparent'};
   margin-right: 0.5rem;
   font-size: smaller;
   white-space: nowrap;
