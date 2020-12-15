1db24dbc72a1ff08a8665e6890ecad7513c8d909
Anxo Rodriguez <anxolin@gmail.com>
2020-12-15 10:11:37 +0100

    Remove unnecesary quotes

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -6,13 +6,13 @@ export * from '@src/constants/index'
 
 // TODO: When contracts are deployed, we can load this from the NPM package
 export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
-  // [ChainId.MAINNET]: GPv2Settlement['1'].address, // Not yet deployed
-  [ChainId.RINKEBY]: GPv2Settlement['4'].address
-  // [ChainId.xDAI]: GPv2Settlement['100'].address // Not yet deployed
+  // [ChainId.MAINNET]: GPv2Settlement[1].address, // Not yet deployed
+  [ChainId.RINKEBY]: GPv2Settlement[4].address
+  // [ChainId.xDAI]: GPv2Settlement[100].address // Not yet deployed
 }
 
 export const GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
-  // [ChainId.MAINNET]: GPv2AllowanceManager['1'].address, // Not yet deployed
-  [ChainId.RINKEBY]: GPv2AllowanceManager['4'].address
-  // [ChainId.xDAI]: GPv2AllowanceManager['100'].address // Not yet deployed
+  // [ChainId.MAINNET]: GPv2AllowanceManager[1].address, // Not yet deployed
+  [ChainId.RINKEBY]: GPv2AllowanceManager[4].address
+  // [ChainId.xDAI]: GPv2AllowanceManager[100].address // Not yet deployed
 }
