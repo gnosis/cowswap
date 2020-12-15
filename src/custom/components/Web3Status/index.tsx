d8f58cca57e495fb0d7ed3c3c90154c5e83b3d8d
David Sato <david.sato64@gmail.com>
2020-12-15 12:18:25 +0100

    remove my custom mock and leave dimas mocks

--- src/custom/components/Web3Status/index.tsx
+++ src/custom/components/Web3Status/index.tsx
@@ -7,12 +7,7 @@ import WalletModal from 'components/WalletModal'
 import { Web3StatusInner } from './Web3StatusMod'
 import useAllTransactionsAndOrders from 'custom/hooks/useAllTransactionsAndOrders'
 
-// TODO: remove once waterfall PRs are done (useful for testing this
-import { useAddOrdersOnMount } from 'custom/state/orders/mock'
-
 export default function Web3Status() {
-  // TODO: remove once waterfall PRs are done (useful for testing this)
-  useAddOrdersOnMount()
   const { active, account } = useWeb3React()
   const contextNetwork = useWeb3React(NetworkContextName)
 
