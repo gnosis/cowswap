2557709421110e873c3fc34477fa81ffd276917d
Anxo Rodriguez <anxolin@gmail.com>
2020-12-15 14:20:02 +0100

    Remove redundant part

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '@uniswap/sdk'
 
-export const APP_ID = Number(process.env.REACT_APP_ID || 0)
+export const APP_ID = Number(process.env.REACT_APP_ID)
 
 // TODO: The addresses will be exported as plain JSON file in the future
 
