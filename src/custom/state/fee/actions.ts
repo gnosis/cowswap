f21e5babc214c7625282aa16915442ddc1094ae1
David Sato <david.sato64@gmail.com>
2020-12-18 10:26:47 +0100

    add chainId to fee actions

--- src/custom/state/fee/actions.ts
+++ src/custom/state/fee/actions.ts
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit'
+import { ChainId } from '@uniswap/sdk'
 import { FeeInformation } from './reducer'
 
-export const updateFee = createAction<{ token: string; fee: FeeInformation }>('fee/updateFee')
-export const clearFee = createAction<{ token: string }>('fee/clearFee')
+export const updateFee = createAction<{ token: string; fee: FeeInformation; chainId: ChainId }>('fee/updateFee')
+export const clearFee = createAction<{ token: string; chainId: ChainId }>('fee/clearFee')
