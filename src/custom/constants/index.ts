898dc780d519f049d8755daab91a0d79aeb11582
David Sato <david.sato64@gmail.com>
2020-12-19 16:37:20 +0100

    added API/GP contract XDAI constants

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -9,12 +9,12 @@ export * from '@src/constants/index'
 // TODO: When contracts are deployed, we can load this from the NPM package
 export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: GPv2Settlement[ChainId.MAINNET].address,
-  [ChainId.RINKEBY]: GPv2Settlement[ChainId.RINKEBY].address
-  // [ChainId.xDAI]: GPv2Settlement[ChainId.xDAI].address // Not yet deployed
+  [ChainId.RINKEBY]: GPv2Settlement[ChainId.RINKEBY].address,
+  [ChainId.XDAI]: GPv2Settlement[ChainId.XDAI].address // Not yet deployed
 }
 
 export const GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: GPv2AllowanceManager[ChainId.MAINNET].address,
-  [ChainId.RINKEBY]: GPv2AllowanceManager[ChainId.RINKEBY].address
-  // [ChainId.xDAI]: GPv2AllowanceManager[ChainId.xDAI].address // Not yet deployed
+  [ChainId.RINKEBY]: GPv2AllowanceManager[ChainId.RINKEBY].address,
+  [ChainId.XDAI]: GPv2AllowanceManager[ChainId.XDAI].address // Not yet deployed
 }
