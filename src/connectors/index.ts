cacaf60541b07fba7903308601be38e8c5058371
Velenir <velenir@gmail.com>
2020-12-21 12:08:08 +0300

    change couple import paths by hand

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -7,7 +7,7 @@ import { PortisConnector } from '@web3-react/portis-connector'
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
 
-import { ChainId } from 'uniswap-xdai-sdk'
+import { ChainId } from '@uniswap/sdk'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { ChainId, JSBI, Percent, Token, WETH } from 'uniswap-xdai-sdk'
+import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
