c15f09ac5df3c4874d87084e22933ec9c03884e7
Velenir <velenir@gmail.com>
2020-12-22 17:53:27 +0300

    extend WETH_ONLY mao

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -57,7 +57,8 @@ const WETH_ONLY: ChainTokenList = {
   [ChainId.ROPSTEN]: [WETH[ChainId.ROPSTEN]],
   [ChainId.RINKEBY]: [WETH[ChainId.RINKEBY]],
   [ChainId.GÖRLI]: [WETH[ChainId.GÖRLI]],
-  [ChainId.KOVAN]: [WETH[ChainId.KOVAN]]
+  [ChainId.KOVAN]: [WETH[ChainId.KOVAN]],
+  [ChainId.XDAI]: [WETH[ChainId.XDAI]]
 }
 
 // used to construct intermediary pairs for trading
