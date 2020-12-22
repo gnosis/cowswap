96ef51f9dd5d176aeb8e893641b63681137daba4
Velenir <velenir@gmail.com>
2020-12-22 17:54:56 +0300

    extend GP_SETTLEMENT_CONTRACT_ADDRESS and GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -1,4 +1,5 @@
-import { ChainId } from '@uniswap/sdk'
+// import { ChainId } from '@uniswap/sdk'
+import { ChainId } from 'xdai'
 import { GPv2Settlement, GPv2AllowanceManager } from '@gnosis.pm/gp-v2-contracts/networks.json'
 
 export const APP_ID = Number(process.env.REACT_APP_ID)
@@ -9,12 +10,12 @@ export * from '@src/constants/index'
 // TODO: When contracts are deployed, we can load this from the NPM package
 export const GP_SETTLEMENT_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: GPv2Settlement[ChainId.MAINNET].address,
-  [ChainId.RINKEBY]: GPv2Settlement[ChainId.RINKEBY].address
-  // [ChainId.xDAI]: GPv2Settlement[ChainId.xDAI].address // Not yet deployed
+  [ChainId.RINKEBY]: GPv2Settlement[ChainId.RINKEBY].address,
+  [ChainId.XDAI]: GPv2Settlement[ChainId.XDAI].address // Not yet deployed
 }
 
 export const GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: GPv2AllowanceManager[ChainId.MAINNET].address,
-  [ChainId.RINKEBY]: GPv2AllowanceManager[ChainId.RINKEBY].address
-  // [ChainId.xDAI]: GPv2AllowanceManager[ChainId.xDAI].address // Not yet deployed
+  [ChainId.RINKEBY]: GPv2AllowanceManager[ChainId.RINKEBY].address,
+  [ChainId.XDAI]: GPv2AllowanceManager[ChainId.XDAI].address // Not yet deployed
 }
