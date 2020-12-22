1ed79aca140ec7c65ffea83cdd1609c9147f1243
Velenir <velenir@gmail.com>
2020-12-22 17:51:55 +0300

    override ROUTER_ADDRESS

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,8 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+// export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x1C232F01118CB8B424793ae03F870aa7D0ac7f77'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
