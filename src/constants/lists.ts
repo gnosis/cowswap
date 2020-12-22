0fe4ce5b5a0f2df2771e5055e07e3f88666ac973
Velenir <velenir@gmail.com>
2020-12-22 17:54:07 +0300

    override DEFAULT_TOKEN_LIST_URL

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,6 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+// export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = 'https://tokens.honeyswap.org'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
