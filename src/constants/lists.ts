86a48b6cd29b251246282fb282386a544a426352
Velenir <velenir@gmail.com>
2020-12-23 15:47:28 +0300

    return DEFAULT_TOKEN_URL back to tokens.uniswap.eth

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,10 +1,11 @@
 // the Uniswap Default token list lives here
 // TODO: make chainId-dependent
-// export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
-export const DEFAULT_TOKEN_LIST_URL = 'https://tokens.honeyswap.org'
+export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL_XDAI = 'https://tokens.honeyswap.org'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
+  DEFAULT_TOKEN_LIST_URL_XDAI,
   't2crtokens.eth', // kleros
   'tokens.1inch.eth', // 1inch
   'synths.snx.eth',
