e2fbd5309fbd4e10fa3666797773d38d54e1af11
Velenir <velenir@gmail.com>
2020-12-23 16:15:50 +0300

    fix tests

--- src/state/lists/reducer.test.ts
+++ src/state/lists/reducer.test.ts
@@ -320,7 +320,7 @@ describe('list reducer', () => {
       store.dispatch(removeList('fake-url'))
       expect(store.getState()).toEqual({
         byUrl: {},
-        selectedListUrl: 'https://tokens.honeyswap.org'
+        selectedListUrl: 'tokens.uniswap.eth'
       })
     })
   })
