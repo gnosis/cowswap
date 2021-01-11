7319230d4d2ef55f6c9f81f0277b63ef87f0b4de
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-01-11 10:19:07 -0800

    Memoizing return of useWrapEther with useCallback

--- src/hooks/useWrapEther.ts
+++ src/hooks/useWrapEther.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { CurrencyAmount } from '@uniswap/sdk'
 
 import { useTransactionAdder } from '@src/state/transactions/hooks'
@@ -7,17 +8,20 @@ import { Contract } from 'ethers'
 export function useWrapEther() {
   const addTransaction = useTransactionAdder()
 
-  return async (amount: CurrencyAmount, weth: Contract): Promise<string> => {
-    console.log('Wrapping ETH!', amount.raw.toString(), weth)
+  return useCallback(
+    async (amount: CurrencyAmount, weth: Contract): Promise<string> => {
+      console.log('Wrapping ETH!', amount.raw.toString(), weth)
 
-    try {
-      const txReceipt = await weth.deposit({ value: `0x${amount.raw.toString(16)}` })
-      addTransaction(txReceipt, { summary: `Wrap ${amount.toSignificant(6)} ETH to WETH` })
-      console.log('Wrapped!', amount)
-      return txReceipt
-    } catch (error) {
-      console.error('Could not WRAP', error)
-      return 'Failed to wrap'
-    }
-  }
+      try {
+        const txReceipt = await weth.deposit({ value: `0x${amount.raw.toString(16)}` })
+        addTransaction(txReceipt, { summary: `Wrap ${amount.toSignificant(6)} ETH to WETH` })
+        console.log('Wrapped!', amount)
+        return txReceipt
+      } catch (error) {
+        console.error('Could not WRAP', error)
+        return 'Failed to wrap'
+      }
+    },
+    [addTransaction]
+  )
 }
