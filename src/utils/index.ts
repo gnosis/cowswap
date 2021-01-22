b10972b8a7a3758072a78da5796240d8a2dc03b1
Anxo Rodriguez <anxolin@gmail.com>
2021-01-22 01:06:33 +0100

    Add blockscout urls

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,14 +22,13 @@ const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
   3: 'ropsten.',
   4: 'rinkeby.',
   5: 'goerli.',
-  42: 'kovan.'
+  42: 'kovan.',
+  100: 'xdai.'
 }
 
-export function getEtherscanLink(
-  chainId: ChainId,
-  data: string,
-  type: 'transaction' | 'token' | 'address' | 'block'
-): string {
+type BlockExplorerLinkType = 'transaction' | 'token' | 'address' | 'block'
+
+function getEtherscanUrl(chainId: ChainId, data: string, type: BlockExplorerLinkType): string {
   const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}etherscan.io`
 
   switch (type) {
@@ -46,6 +45,41 @@ export function getEtherscanLink(
     default: {
       return `${prefix}/address/${data}`
     }
+  }
+}
+
+function getBlockscoutUrlPrefix(chainId: ChainId): string {
+  switch (chainId) {
+    case ChainId.XDAI:
+      return 'poa/xdai'
+
+    default:
+      return ''
+  }
+}
+
+function getBlockscoutUrlSuffix(type: BlockExplorerLinkType, data: string): string {
+  switch (type) {
+    case 'transaction':
+      return `tx/${data}`
+    case 'block':
+      return `blocks/${data}/transactions`
+    case 'address':
+      return `address/${data}/transactions`
+    case 'token':
+      return `tokens/${data}/token-transfers`
+  }
+}
+
+function getBlockscoutUrl(chainId: ChainId, data: string, type: BlockExplorerLinkType): string {
+  return `https://blockscout.com/${getBlockscoutUrlPrefix(chainId)}/${getBlockscoutUrlSuffix(type, data)}`
+}
+
+export function getEtherscanLink(chainId: ChainId, data: string, type: BlockExplorerLinkType): string {
+  if (chainId === ChainId.XDAI) {
+    return getBlockscoutUrl(chainId, data, type)
+  } else {
+    return getEtherscanUrl(chainId, data, type)
   }
 }
 
