435c1c29c71a7171bf29acd7a07c598bcff1bcf6
Anxo Rodriguez <anxolin@gmail.com>
2021-01-25 09:48:13 +0100

    Keep empty line

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 import { fortmatic, injected, portis, walletconnect, walletlink } from 'connectors'
 
 export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 // a list of tokens by chain
