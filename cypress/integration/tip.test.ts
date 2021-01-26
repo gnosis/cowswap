37ff3e86e9a8f73d607a12af473ccfce8b72ab7a
David Sato <david.sato64@gmail.com>
2021-01-26 08:32:46 +0000

    updates

--- cypress/integration/tip.test.ts
+++ cypress/integration/tip.test.ts
@@ -1,22 +1,28 @@
-import { Currency, CurrencyAmount, JSBI, Token, TokenAmount, Trade } from '@uniswap/sdk'
+import { ChainId, Currency, CurrencyAmount, ETHER, JSBI, Token, TokenAmount, Trade, WETH } from '@uniswap/sdk'
 import { getFeeAmount } from '../../src/custom/utils/fee'
-import { checkIfEther } from '../../src/custom/utils/operator'
+
+function checkIfEther(tokenAddress: string, chainId: ChainId) {
+  let checkedAddress = tokenAddress
+  if (tokenAddress === ETHER.symbol) {
+    checkedAddress = WETH[chainId].address
+  }
+
+  return checkedAddress
+}
 
 const stringToCurrency = (amount: string, currency: Currency) =>
   currency instanceof Token ? new TokenAmount(currency, JSBI.BigInt(amount)) : CurrencyAmount.ether(JSBI.BigInt(amount))
 
 // Assumptions:
-// MAINNET
+// RINKEBY
 // SELL: ETH
 // BUY: DAI
 const DAI = '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735'
 const INPUT_TOKEN = 'ETH'
 const OUTPUT_TOKEN = DAI
 
-const INPUT_AMOUNT = '50'
-
-const FEE_QUERY = `https://protocol-mainnet.dev.gnosisdev.com/api/v1/tokens/${checkIfEther(INPUT_TOKEN, 1)}/fee`
-
+const INPUT_AMOUNT = '10'
+const FEE_QUERY = `https://protocol-rinkeby.dev.gnosisdev.com/api/v1/tokens/${checkIfEther(INPUT_TOKEN, 4)}/fee`
 const SWAP_URL = `/swap?inputCurrency=${INPUT_TOKEN}&outputCurrency=${OUTPUT_TOKEN}`
 
 describe('Swap::Trade::Tip', () => {
@@ -34,18 +40,20 @@ describe('Swap::Trade::Tip', () => {
   })
 
   it('Swap ETH for DAI and applies fee', () => {
+    // Open currency selector
     cy.get('#swap-currency-output .open-currency-select-button').click()
     cy.get(`.token-item-${DAI}`).should(`be.visible`)
+    // select DAI
     cy.get(`.token-item-${DAI}`).click({ force: true })
     cy.get('#swap-currency-input .token-amount-input').should('be.visible')
+    // input the cached input amount (above)
     cy.get('#swap-currency-input .token-amount-input').type(INPUT_AMOUNT, { force: true, delay: 200 })
     cy.get('#swap-currency-output .token-amount-input').should('not.equal', '')
 
-    // eslint-disable-next-line prettier/prettier
-    cy.wait(2500)
-    .then(() => {
+    cy.wait(2500).then(() => {
       // check fee
       cy.request(FEE_QUERY).then(response => {
+        // we expect a returned fee amount
         expect(response).to.have.property('body')
         expect(response.body).to.have.property('minimalFee') // true
         expect(response.body).to.have.property('feeRatio') // true
@@ -60,12 +68,13 @@ describe('Swap::Trade::Tip', () => {
           const trade: Trade = win.trade
           const { inputAmount, outputAmount, executionPrice } = trade
 
+          // can test with:
+          // minimalFee: (20 * 10 ** 18).toString(),
+          // feeRatio: 9000,
+          // sellAmount: inputAmount.raw.toString()
           const fee = getFeeAmount({
+            sellAmount: inputAmount.raw.toString(),
             ...response.body
-            // can test with:
-            // minimalFee: (20 * 10 ** 18).toString(),
-            // feeRatio: 9000,
-            // sellAmount: inputAmount.raw.toString()
           })
 
           // get the fee as a CurrencyAmount
