3152bf2f6cb8fe99666d09264805d4cd74a195ef
David Sato <david.sato64@gmail.com>
2021-02-01 15:48:16 +0100

    test logs

--- src/custom/state/fee/updater.ts
+++ src/custom/state/fee/updater.ts
@@ -9,6 +9,13 @@ import { getFeeQuote } from 'utils/operator'
 import { registerOnWindow } from '@src/custom/utils/misc'
 
 function isDateLater(dateA: string, dateB: string): boolean {
+  console.debug(
+    `
+[FEE STORAGE]::[FEE UPDATER]::
+[FEE DATE]::${new Date(Date.parse(dateA))},
+[NOW DATE]::${new Date(Date.parse(dateB))}
+    `
+  )
   const [parsedDateA, parsedDateB] = [Date.parse(dateA), Date.parse(dateB)]
 
   return parsedDateA > parsedDateB
@@ -41,8 +48,14 @@ export default function FeesUpdater(): null {
     }) {
       const currentFee = feesMap[sellToken]?.fee
       const isFeeDateValid = currentFee && isDateLater(currentFee.expirationDate, now)
+      console.debug(`
+==================================================
+[FEE STORAGE]::[IS DATE VALID?] => ${isFeeDateValid}
+==================================================
+      `)
 
       if (!isFeeDateValid || !currentFee) {
+        console.debug('[FEE STORAGE]::[QUERYING]')
         const fee = await getFeeQuote(chainId, sellToken).catch(err => {
           console.error(new Error(err))
           return null
