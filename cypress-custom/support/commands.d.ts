1672e9099a3d8619eabfae049086c1d7e69ab3c3
David Sato <david.sato64@gmail.com>
2021-02-02 12:33:57 +0100

    add stub command

--- cypress-custom/support/commands.d.ts
+++ cypress-custom/support/commands.d.ts
@@ -1,12 +1,19 @@
 /// <reference types="cypress" />
 
 declare namespace Cypress {
-  interface Chainable {
+  interface Chainable<Subject> {
     /**
      * Select token input in the swap page
      *
      * @example cy.selectTokens()
      */
-    swapSelectInput(tokenAddress: string): Chainable<Element>
+    swapSelectInput(tokenAddress: string): Chainable<Subject>
+
+    /**
+     * Set a stubbing intercept on route specified
+     *
+     * @example cy.swapStubResponse({ query: '/api/v1/someEndpoint/', alias: 'endpoint', body: { foo: 'foo' } })
+     */
+    swapStubResponse({ query, alias, body }: { query: string; alias?: string; body?: any }): Chainable<Subject>
   }
 }
