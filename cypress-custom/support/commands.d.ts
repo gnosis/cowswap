748ce2eecca5d943f349fe37222ede262597d183
David Sato <david.sato64@gmail.com>
2021-02-02 12:17:43 +0100

    added commands

--- cypress-custom/support/commands.d.ts
+++ cypress-custom/support/commands.d.ts
@@ -5,9 +5,21 @@ declare namespace Cypress {
     /**
      * Select token input in the swap page
      *
-     * @example cy.selectTokens()
+     * @example cy.swapSelectInput()
      */
-    swapSelectInput(tokenAddress: string): Chainable<Subject>
+    swapSelectToken(tokenAddress: string, type: string): Chainable<Subject>
+
+    swapInputCheckOutput({
+      inputName,
+      outputName,
+      typedAmount,
+      expectedOutput
+    }: {
+      inputName: string
+      outputName: string
+      typedAmount?: string | null
+      expectedOutput: string
+    }): Chainable<Subject>
 
     /**
      * Set a stubbing intercept on route specified
@@ -15,5 +27,22 @@ declare namespace Cypress {
      * @example cy.stubResponse({ url: '/api/v1/someEndpoint/', alias: 'endpoint', body: { foo: 'foo' } })
      */
     stubResponse({ url, alias, body }: { url: string; alias?: string; body?: any }): Chainable<Subject>
+
+    /**
+     * Set a stubbing intercept on route specified and visit app
+     *
+     * @example cy.swapStubAndVisit({ query: '/api/v1/someEndpoint/', alias: 'endpoint', body: { foo: 'foo' }, visit: '/home' })
+     */
+    stubAndVisit({
+      query,
+      alias,
+      body,
+      visit
+    }: {
+      query: string
+      alias?: string
+      body?: any
+      visit: string
+    }): Chainable<Subject>
   }
 }
--- cypress-custom/support/commands.js
+++ cypress-custom/support/commands.js
@@ -1,7 +1,16 @@
-Cypress.Commands.add('swapSelectInput', tokenAddress => {
-  cy.get('#swap-currency-input .open-currency-select-button').click()
+Cypress.Commands.add('swapSelectToken', (tokenAddress, type = 'input') => {
+  cy.get(`#swap-currency-${type} .open-currency-select-button`).click()
   cy.get('.token-item-' + tokenAddress).should('be.visible')
   cy.get('.token-item-' + tokenAddress).click({ force: true })
+})
+
+Cypress.Commands.add('swapInputCheckOutput', ({ inputName, outputName, typedAmount, expectedOutput }) => {
+  cy.get(inputName)
+    .should('exist')
+    .type(typedAmount, { force: true, delay: 200 })
+  cy.get(outputName)
+    .should('exist')
+    .should('have.value', expectedOutput)
 })
 
 function _responseHandlerFactory(body) {
@@ -14,4 +23,9 @@ function _responseHandlerFactory(body) {
 
 Cypress.Commands.add('stubResponse', ({ url, alias = 'stubbedResponse', body }) => {
   cy.route2({ method: 'GET', url }, _responseHandlerFactory(body)).as(alias)
+})
+
+Cypress.Commands.add('stubAndVisit', ({ query, alias = 'stubbedResponse', body, visit = '/' }) => {
+  cy.swapStubResponse({ query, alias, body, visit })
+  cy.visit(visit)
 })
