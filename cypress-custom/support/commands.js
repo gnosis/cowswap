6ba5e9f2c77634b887bbe009ff364f8a18e447ed
David Sato <david.sato64@gmail.com>
2021-02-03 14:58:10 +0100

    simplified stub

--- cypress-custom/support/commands.js
+++ cypress-custom/support/commands.js
@@ -4,14 +4,6 @@ Cypress.Commands.add('swapSelectInput', tokenAddress => {
   cy.get('.token-item-' + tokenAddress).click({ force: true })
 })
 
-function _responseHandlerFactory(body) {
-  return req =>
-    req.reply(res => {
-      const newBody = JSON.stringify(body || res.body)
-      res.body = newBody
-    })
-}
-
-Cypress.Commands.add('stubResponse', ({ query, alias = 'stubbedResponse', body }) => {
-  cy.route2('GET', query, _responseHandlerFactory(body)).as(alias)
+Cypress.Commands.add('stubResponse', ({ url, alias = 'stubbedResponse', body }) => {
+  cy.route2(url, { body }).as(alias)
 })
