88ca0bfacce174664d7931208f765c400d3035ab
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-02-11 14:23:48 -0800

    Removed trailling slash

--- src/custom/utils/explorer.ts
+++ src/custom/utils/explorer.ts
@@ -3,7 +3,7 @@ import { OrderID } from 'utils/operator'
 
 // TODO: using dev endpoint for now. Update to staging/prod when available
 const EXPLORER_BASE_URL: Partial<Record<ChainId, string>> = {
-  [ChainId.MAINNET]: 'https://protocol-explorer.dev.gnosisdev.com/',
+  [ChainId.MAINNET]: 'https://protocol-explorer.dev.gnosisdev.com',
   [ChainId.RINKEBY]: 'https://protocol-explorer.dev.gnosisdev.com/rinkeby',
   [ChainId.XDAI]: 'https://protocol-explorer.dev.gnosisdev.com/xdai'
 }
