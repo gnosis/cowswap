7be10492e970bbcad632a83569e6e7b68c5de003
David Sato <david.sato64@gmail.com>
2021-02-23 16:56:52 +0000

    create NetworkListUpdater to manage lists and chains
    1. saves token lists to storage and appends chainId
    2. allows easier retrieval of lists when switching

---
+++ src/custom/state/lists/updater.tsx
@@ -0,0 +1,113 @@
+import React, { useEffect } from 'react'
+import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
+import { ChainId } from '@uniswap/sdk'
+import { useDispatch } from 'react-redux'
+
+import { useActiveWeb3React } from '@src/hooks'
+
+import { DEFAULT_ACTIVE_LIST_URLS_BY_NETWORK, DEFAULT_LIST_OF_LISTS_BY_NETWORK } from 'constants/lists'
+import { initialiseTokenLists } from './actions'
+import { ListsState, NEW_LIST_STATE } from './reducer'
+import { buildListState } from './helpers'
+
+const BASE_STORAGE_KEY = 'redux_localstorage_simple_lists'
+
+function buildKey(chainId?: ChainId) {
+  const identifier = chainId ? `_${chainId}` : ''
+
+  return BASE_STORAGE_KEY + identifier
+}
+
+export const NetworkListUpdater: React.FC = () => {
+  const { chainId } = useActiveWeb3React()
+  // const chainIdRef = useRef(chainId)
+
+  const dispatch = useDispatch()
+
+  // Loading//Saving network specific lists from//into storage
+  useEffect(() => {
+    if (chainId) {
+      console.debug('HERE')
+      let refreshedTokenState: ListsState
+      // build identifier for storage token list e.g Rinkeby list identifier = "_4"
+      const newChainKey = buildKey(chainId)
+      // Get the localStorage token list saved under identifer key
+      // e.g "redux_localstorage_simple_lists_4"
+      const storageListJSON = localStorage.getItem(newChainKey)
+
+      // storageListJSON && localStorage.setItem(newChainKey, storageListJSON)
+      if (storageListJSON) {
+        // We have a storage token list
+        // but what if it differs from the defaults?
+        const oldStorageList = JSON.parse(storageListJSON)
+
+        // cache the network specific defaults from 'custom/constants/lists'
+        // access using chainId
+        const newListOfLists = DEFAULT_LIST_OF_LISTS_BY_NETWORK[chainId]
+        const newActiveListUrls = DEFAULT_ACTIVE_LIST_URLS_BY_NETWORK[chainId]
+
+        // Make a copy of the previous storage list
+        const tokenStateCopy = Object.assign({}, oldStorageList)
+
+        // we need to compare the lists and merge/cleanup
+        // lets start with the last initialised list of lists
+        if (oldStorageList.lastInitializedDefaultListOfLists) {
+          // Lets create a Set of the last initialised list of lists saved, to compare
+          const oldStorageLastInitializedSet: Set<string> = new Set(tokenStateCopy.lastInitializedDefaultListOfLists)
+          // and a set of the (potentially) new ones from 'custom/constants/lists'
+          const newListofListsSet = new Set(newListOfLists)
+
+          // lets loop over the 'constants/lists' listsOfLists and check the
+          // oldStorageLastInitializedSet vs the storage list one
+          newListofListsSet.forEach(newListUrl => {
+            // if storage token list doesn't have a token list URL
+            // that the new default does, we add it
+            if (!oldStorageLastInitializedSet.has(newListUrl)) {
+              tokenStateCopy.byUrl[newListUrl] = NEW_LIST_STATE
+            }
+          })
+
+          // check the previous lists "lastInitialisedTokens" against the incoming list
+          oldStorageLastInitializedSet.forEach((oldListUrl: string) => {
+            // remove any token list urls that the default list doesn't have
+            if (!newListofListsSet.has(oldListUrl)) {
+              delete tokenStateCopy.byUrl[oldListUrl]
+            }
+          })
+
+          // if no active lists, activate defaults
+          if (!tokenStateCopy.activeListUrls) {
+            tokenStateCopy.activeListUrls = newActiveListUrls
+
+            // for each list on default list, initialize if needed
+            newActiveListUrls.map((listUrl: string) => {
+              if (!tokenStateCopy.byUrl[listUrl]) {
+                tokenStateCopy.byUrl[listUrl] = NEW_LIST_STATE
+              }
+              return true
+            })
+          }
+        }
+
+        tokenStateCopy.lastInitializedDefaultListOfLists = newListOfLists
+
+        refreshedTokenState = tokenStateCopy
+      } else {
+        // clean slate, build defaults
+        refreshedTokenState = buildListState({
+          defaultActiveListUrls: DEFAULT_ACTIVE_LIST_URLS_BY_NETWORK[chainId],
+          defaultListofLists: DEFAULT_LIST_OF_LISTS_BY_NETWORK[chainId],
+          newListState: NEW_LIST_STATE
+        })
+      }
+
+      batchedUpdates(() => {
+        // save chain aware tokens to localStorage and redux
+        dispatch(initialiseTokenLists(refreshedTokenState))
+        localStorage.setItem(newChainKey, JSON.stringify(refreshedTokenState))
+      })
+    }
+  }, [chainId, dispatch])
+
+  return null
+}
