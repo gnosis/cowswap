467a0f6ec3f5ff8002aac482a8e970d1dcacf659
David Sato <david.sato64@gmail.com>
2021-02-26 13:47:57 +0000

    clean up logic

--- src/custom/state/lists/helpers.ts
+++ src/custom/state/lists/helpers.ts
@@ -1,5 +1,7 @@
+import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
 import { ChainId } from '@uniswap/sdk'
-import { ListsState, ListState, Mutable } from './reducer'
+import { initialiseTokenLists } from './actions'
+import { ListsState, ListState, Mutable, NEW_LIST_STATE } from './reducer'
 
 export function buildListState({
   defaultListofLists,
@@ -28,4 +30,67 @@ export function buildKey(chainId?: ChainId) {
   const identifier = chainId ? `_${chainId}` : ''
 
   return BASE_STORAGE_KEY + identifier
+}
+
+export function _getCopyOfStorageLists(listJSON: string) {
+  // We have a storage token list
+  // but what if it differs from the defaults?
+  const oldStorageList = JSON.parse(listJSON)
+  // Make a copy of the previous storage list
+  return Object.assign({}, oldStorageList)
+}
+interface TokenListsParams<T> {
+  staleTokenLists: T
+  newTokenLists: string[]
+}
+
+export function _removeStaleTokenLists({
+  staleTokenLists,
+  newTokenLists
+}: TokenListsParams<Mutable<ListsState['byUrl']>>) {
+  const newUrlsSet = new Set(newTokenLists)
+
+  // for each list on default list, remove if needed
+  const updatedByUrls = Object.keys(staleTokenLists).reduce((acc, oldUrl) => {
+    if (!newUrlsSet.has(oldUrl)) {
+      return acc
+    }
+
+    return staleTokenLists
+  }, {} as typeof staleTokenLists)
+
+  return updatedByUrls
+}
+
+export function _addNewTokenLists({ staleTokenLists, newTokenLists }: TokenListsParams<Mutable<ListsState['byUrl']>>) {
+  const staleUrlsSet = new Set(Object.keys(staleTokenLists))
+
+  // for each list on default list, initialize if needed
+  const updatedByUrls = newTokenLists.reduce((acc, newUrl) => {
+    if (!staleUrlsSet.has(newUrl)) {
+      acc[newUrl] = NEW_LIST_STATE
+    }
+    return acc
+  }, {} as typeof staleTokenLists)
+
+  return {
+    ...staleTokenLists,
+    ...updatedByUrls
+  }
+}
+
+export function _updateState({
+  storageKey,
+  state,
+  dispatch
+}: {
+  storageKey: string
+  state: ListsState
+  dispatch: React.Dispatch<any>
+}) {
+  return batchedUpdates(() => {
+    // save chain aware tokens to localStorage and redux
+    dispatch(initialiseTokenLists(state))
+    localStorage.setItem(storageKey, JSON.stringify(state))
+  })
 }
--- src/custom/state/lists/updater.tsx
+++ src/custom/state/lists/updater.tsx
@@ -1,88 +1,82 @@
 import { useEffect } from 'react'
-import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
 import { useDispatch } from 'react-redux'
 
 import { useActiveWeb3React } from '@src/hooks'
 
 import { DEFAULT_ACTIVE_LIST_URLS_BY_NETWORK, DEFAULT_LIST_OF_LISTS_BY_NETWORK } from 'constants/lists'
-import { initialiseTokenLists } from './actions'
-import { ListsState, NEW_LIST_STATE } from './reducer'
-import { buildKey, buildListState } from './helpers'
+import { ListsState, Mutable, NEW_LIST_STATE } from './reducer'
+import {
+  buildKey,
+  buildListState,
+  _addNewTokenLists,
+  _getCopyOfStorageLists,
+  _removeStaleTokenLists,
+  _updateState
+} from './helpers'
 
 export default function NetworkListUpdater(): null {
   const { chainId } = useActiveWeb3React()
-  // const chainIdRef = useRef(chainId)
 
   const dispatch = useDispatch()
 
   // Loading//Saving network specific lists from//into storage
   useEffect(() => {
     if (chainId) {
-      let refreshedTokenState: ListsState
+      let refreshedTokenState: Mutable<ListsState>
       // build identifier for storage token list e.g Rinkeby list identifier = "_4"
       const newChainKey = buildKey(chainId)
+      // New token lists to compare against
+      const newTokenLists = DEFAULT_LIST_OF_LISTS_BY_NETWORK[chainId]
+      const newActiveListUrls = DEFAULT_ACTIVE_LIST_URLS_BY_NETWORK[chainId]
       // Get the localStorage token list saved under identifer key
       // e.g "redux_localstorage_simple_lists_4"
-      const storageListJSON = localStorage.getItem(newChainKey)
+      const tokenListsJSON = localStorage.getItem(newChainKey)
 
-      // storageListJSON && localStorage.setItem(newChainKey, storageListJSON)
-      if (storageListJSON) {
-        // We have a storage token list
-        // but what if it differs from the defaults?
-        const oldStorageList = JSON.parse(storageListJSON)
-
-        // cache the network specific defaults from 'custom/constants/lists'
-        // access using chainId
-        const newListOfLists = DEFAULT_LIST_OF_LISTS_BY_NETWORK[chainId]
-        const newActiveListUrls = DEFAULT_ACTIVE_LIST_URLS_BY_NETWORK[chainId]
-
-        // Make a copy of the previous storage list
-        const tokenStateCopy = Object.assign({}, oldStorageList)
+      if (tokenListsJSON) {
+        refreshedTokenState = _getCopyOfStorageLists(tokenListsJSON)
 
         // we need to compare the lists and merge/cleanup
-        // lets start with the last initialised list of lists
-        if (oldStorageList.lastInitializedDefaultListOfLists) {
-          // Lets create a Set of the last initialised list of lists saved, to compare
-          const oldStorageLastInitializedSet: Set<string> = new Set(tokenStateCopy.lastInitializedDefaultListOfLists)
-          // and a set of the (potentially) new ones from 'custom/constants/lists'
-          const newListofListsSet = new Set(newListOfLists)
+        // using the last initialised list as a comparison, we can initialise/remove
+        if (refreshedTokenState.lastInitializedDefaultListOfLists) {
+          // add any new token lists from DEFAULT_LISTS_OF_LISTS
+          const newByUrlTokens = _addNewTokenLists({
+            staleTokenLists: refreshedTokenState.byUrl,
+            newTokenLists
+          })
+          // remove stale found in previous lastInitialisedListsOfLists
+          const refreshedByUrlTokenLists = _removeStaleTokenLists({ staleTokenLists: newByUrlTokens, newTokenLists })
 
-          // lets loop over the 'constants/lists' listsOfLists and check the
-          // oldStorageLastInitializedSet vs the storage list one
-          newListofListsSet.forEach(newListUrl => {
-            // if storage token list doesn't have a token list URL
-            // that the new default does, we add it
-            if (!oldStorageLastInitializedSet.has(newListUrl)) {
-              tokenStateCopy.byUrl[newListUrl] = NEW_LIST_STATE
+          // merge the lists
+          refreshedTokenState = {
+            ...refreshedTokenState,
+            byUrl: {
+              ...refreshedTokenState.byUrl,
+              ...refreshedByUrlTokenLists
             }
+          }
+        }
+
+        // if no active lists, activate defaults
+        // if active token lists don't yet exist in byUrl, update
+        if (!refreshedTokenState.activeListUrls) {
+          refreshedTokenState.activeListUrls = newActiveListUrls
+
+          const updatedFromActiveListUrls = _addNewTokenLists({
+            staleTokenLists: refreshedTokenState.byUrl,
+            newTokenLists: newActiveListUrls
           })
 
-          // check the previous lists "lastInitialisedTokens" against the incoming list
-          oldStorageLastInitializedSet.forEach((oldListUrl: string) => {
-            // remove any token list urls that the default list doesn't have
-            if (!newListofListsSet.has(oldListUrl)) {
-              delete tokenStateCopy.byUrl[oldListUrl]
+          refreshedTokenState = {
+            ...refreshedTokenState,
+            byUrl: {
+              ...refreshedTokenState.byUrl,
+              ...updatedFromActiveListUrls
             }
-          })
-
-          // if no active lists, activate defaults
-          if (!tokenStateCopy.activeListUrls) {
-            tokenStateCopy.activeListUrls = newActiveListUrls
-
-            // for each list on default list, initialize if needed
-            newActiveListUrls.map((listUrl: string) => {
-              if (!tokenStateCopy.byUrl[listUrl]) {
-                tokenStateCopy.byUrl[listUrl] = NEW_LIST_STATE
-              }
-              return true
-            })
           }
         }
 
         // set our state's last initialised to the new list
-        tokenStateCopy.lastInitializedDefaultListOfLists = newListOfLists
-
-        refreshedTokenState = tokenStateCopy
+        refreshedTokenState.lastInitializedDefaultListOfLists = newTokenLists
       } else {
         // clean slate, build defaults
         refreshedTokenState = buildListState({
@@ -92,10 +86,11 @@ export default function NetworkListUpdater(): null {
         })
       }
 
-      batchedUpdates(() => {
-        // save chain aware tokens to localStorage and redux
-        dispatch(initialiseTokenLists(refreshedTokenState))
-        localStorage.setItem(newChainKey, JSON.stringify(refreshedTokenState))
+      // Save everything
+      _updateState({
+        state: refreshedTokenState,
+        storageKey: newChainKey,
+        dispatch
       })
     }
   }, [chainId, dispatch])
