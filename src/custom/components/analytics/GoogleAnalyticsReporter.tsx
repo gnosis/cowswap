9066cc8d12aa4d6635a0adc56502fbc0af2cd647
Anxo Rodriguez <anxolin@gmail.com>
2021-03-03 15:01:25 +0100

    Fix mainnet case

--- src/custom/components/analytics/GoogleAnalyticsReporter.tsx
+++ src/custom/components/analytics/GoogleAnalyticsReporter.tsx
@@ -2,16 +2,21 @@ import { useActiveWeb3React } from '@src/hooks'
 import { useEffect } from 'react'
 import ReactGA from 'react-ga'
 import { RouteComponentProps } from 'react-router-dom'
-import { NETWORK_LABELS } from 'components/Header'
+import { NETWORK_LABELS as NETWORK_LABELS_HEADER } from 'components/Header'
+import { ChainId } from '@uniswap/sdk'
 
 const NETWORK_DIMENSION = 'dimension1'
+export const NETWORK_LABELS: { [chainId in ChainId]?: string } = {
+  ...NETWORK_LABELS_HEADER,
+  [ChainId.MAINNET]: 'Mainnet' // The header don't has Mainet
+}
 
 export default function GoogleAnalyticsReporter({ location: { pathname, search } }: RouteComponentProps): null {
   const { chainId } = useActiveWeb3React()
 
   // Update the GA network dimension
   useEffect(() => {
-    const networkInfo = chainId ? NETWORK_LABELS[chainId] : 'Not connected'
+    const networkInfo = (chainId && NETWORK_LABELS[chainId]) || 'Not connected'
     ReactGA.set({
       [NETWORK_DIMENSION]: networkInfo
     })
