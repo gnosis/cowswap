ddeaec53b37cf835e2aca96c2f54d4d44eb8d752
Anxo Rodriguez <anxolin@gmail.com>
2021-03-05 14:56:07 +0100

    Define our own custom tests

--- cypress-custom/integration/swap.test.ts
+++ cypress-custom/integration/swap.test.ts
@@ -1,30 +1,14 @@
+import './swapMod.test'
+
 // MOD of original Uni Swap test
 // Main differences summarised:
 // GP doesn't use ETH, so we need to test for this
 
-describe('Swap', () => {
+describe('Swap (custom)', () => {
   beforeEach(() => {
     cy.visit('/swap')
   })
-  it('can enter an amount into input', () => {
-    cy.get('#swap-currency-input .token-amount-input')
-      .type('0.001', { delay: 200 })
-      .should('have.value', '0.001')
-  })
 
-  it('invalid swap amount', () => {
-    cy.get('#swap-currency-input .token-amount-input')
-      .type('\\', { delay: 200 })
-      .should('have.value', '')
-  })
-
-  it('zero output amount', () => {
-    cy.get('#swap-currency-output .token-amount-input')
-      .type('0.0', { delay: 200 })
-      .should('have.value', '0.0')
-  })
-
-  // MOD
   // uses WETH instead of ETH
   // it('can swap ETH for DAI', () => {
   it('can swap WETH for DAI', () => {
@@ -40,7 +24,6 @@ describe('Swap', () => {
     cy.get('#confirm-swap-or-send').should('contain', 'Confirm Swap')
   })
 
-  // MOD
   // ETH should NOT be tradable
   it('cannot swap ETH for DAI', () => {
     // select ETH
@@ -58,35 +41,5 @@ describe('Swap', () => {
     cy.get('#swap-button')
       .should('be.disabled')
       .should('contain', 'ETH cannot be traded. Use WETH')
-  })
-
-  it('add a recipient does not exist unless in expert mode', () => {
-    cy.get('#add-recipient-button').should('not.exist')
-  })
-
-  describe('expert mode', () => {
-    beforeEach(() => {
-      cy.window().then(win => {
-        cy.stub(win, 'prompt').returns('confirm')
-      })
-      cy.get('#open-settings-dialog-button').click()
-      cy.get('#toggle-expert-mode-button').click()
-      cy.get('#confirm-expert-mode').click()
-    })
-
-    it('add a recipient is visible', () => {
-      cy.get('#add-recipient-button').should('be.visible')
-    })
-
-    it('add a recipient', () => {
-      cy.get('#add-recipient-button').click()
-      cy.get('#recipient').should('exist')
-    })
-
-    it('remove recipient', () => {
-      cy.get('#add-recipient-button').click()
-      cy.get('#remove-recipient-button').click()
-      cy.get('#recipient').should('not.exist')
-    })
   })
 })
