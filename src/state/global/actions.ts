d7b282007d8e963bf7354e7cad99648176059ac9
David Sato <david.sato64@gmail.com>
2021-03-12 23:30:52 +0000

    Mod: state/global/actions: add chainId

--- src/state/global/actions.ts
+++ src/state/global/actions.ts
@@ -1,5 +1,7 @@
 import { createAction } from '@reduxjs/toolkit'
+// import { DEFAULT_NETWORK_FOR_LISTS } from '@src/custom/constants/lists'
+import { ChainId } from '@uniswap/sdk'
 
 // fired once when the app reloads but before the app renders
 // allows any updates to be applied to store data loaded from localStorage
-export const updateVersion = createAction<void>('global/updateVersion')
+export const updateVersion = createAction<{ chainId?: ChainId }>('global/updateVersion')
