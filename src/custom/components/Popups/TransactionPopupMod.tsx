93eb892e5bb5545fe986a77551c187d373543ff3
David Sato <david.sato64@gmail.com>
2021-03-15 18:34:53 +0000

    fix toast message
    1. revert back to original code
    2. construct dynamic vars

--- src/custom/components/Popups/TransactionPopupMod.tsx
+++ src/custom/components/Popups/TransactionPopupMod.tsx
@@ -1,13 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { AlertCircle, CheckCircle } from 'react-feather'
 import styled, { ThemeContext } from 'styled-components'
 import { useActiveWeb3React } from 'hooks'
-import { TYPE } from 'theme'
-// import { ExternalLink } from 'theme'
-// import { getEtherscanLink } from 'utils'
+import { TYPE, ExternalLink } from 'theme'
 import { AutoColumn } from 'components/Column'
 import { AutoRow } from 'components/Row'
-import { ExplorerLink } from '../ExplorerLink'
+import { getEtherscanLink, getExplorerLabel } from 'utils'
 
 const RowNoFlex = styled(AutoRow)`
   flex-wrap: nowrap;
@@ -26,17 +24,23 @@ export default function TransactionPopup({
 
   const theme = useContext(ThemeContext)
 
+  const [transactionUrl, hashToDisplay, transactionLabel] = useMemo(
+    () => [
+      chainId && getEtherscanLink(chainId, hash, 'transaction'),
+      hash.slice(0, 8) + '...' + hash.slice(58, 65),
+      chainId && getExplorerLabel(chainId, hash, 'transaction')
+    ],
+    [chainId, hash]
+  )
+
   return (
     <RowNoFlex>
       <div style={{ paddingRight: 16 }}>
         {success ? <CheckCircle color={theme.green1} size={24} /> : <AlertCircle color={theme.red1} size={24} />}
       </div>
       <AutoColumn gap="8px">
-        <TYPE.body fontWeight={500}>{summary ?? 'Hash: ' + hash.slice(0, 8) + '...' + hash.slice(58, 65)}</TYPE.body>
-        {chainId && (
-          // <ExternalLink href={getEtherscanLink(chainId, hash, 'transaction')}>View on Etherscan</ExternalLink>
-          <ExplorerLink id={hash} />
-        )}
+        <TYPE.body fontWeight={500}>{summary ?? 'Hash: ' + hashToDisplay}</TYPE.body>
+        {transactionUrl && <ExternalLink href={transactionUrl}>{transactionLabel}</ExternalLink>}
       </AutoColumn>
     </RowNoFlex>
   )
