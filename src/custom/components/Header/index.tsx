31d8bb4b5121c3d326b16234014643843d2b68ac
Anxo Rodriguez <anxolin@gmail.com>
2021-03-19 17:45:25 +0100

    Move appStatus label out of the mod

--- src/custom/components/Header/index.tsx
+++ src/custom/components/Header/index.tsx
@@ -1,13 +1,31 @@
+import React from 'react'
 import HeaderMod, { UniIcon } from './HeaderMod'
 import styled from 'styled-components'
+import { status as appStatus } from '@src/../package.json'
+
 export { NETWORK_LABELS } from './HeaderMod'
 
-export const Header = styled(HeaderMod)`
+export const HeaderModWrapper = styled(HeaderMod)`
   border-bottom: ${({ theme }) => (theme.header?.border ? theme.header.border : `1px solid ${theme.border}`)};
 
   ${UniIcon} {
     display: flex;
   }
+`
+
+const AppStatusWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 8px;
+  background: white;
+  border: 2px solid ${({ theme }) => theme.primary1};
+  color: ${({ theme }) => theme.primary1};
+  margin: auto 0 -5px -5px;
+  padding: 3px 6px;
+  font-weight: 800;
+  letter-spacing: 0.2rem;
+  text-transform: uppercase;
 `
 
 export const LogoImage = styled.img.attrs(props => ({
@@ -19,4 +37,6 @@ export const LogoImage = styled.img.attrs(props => ({
   object-fit: contain;
 `
 
-export default Header
+export default function Header() {
+  return <HeaderModWrapper statusLabel={<AppStatusWrapper>{appStatus}</AppStatusWrapper>} />
+}
