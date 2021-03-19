af5934745b134d2fb922de04173e1036a6575804
Anxo Rodriguez <anxolin@gmail.com>
2021-03-19 16:06:26 +0100

    Create mod file and override

---
+++ src/custom/components/CurrencyLogo/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Currency } from '@uniswap/sdk'
+import CurrencyLogoMod, { getTokenLogoURL as getTokenLogoURLUni } from './CurrencyLogoMod'
+export * from './CurrencyLogoMod'
+
+/**
+ * Just some basic overrides as a temporal solution to show Rinkeby/xDAI token images
+ */
+const addressOverrides: Map<string, string> = new Map([
+  ['0xc778417e063141139fce010982780140aa0cd5ab', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2']
+])
+
+function getTokenLogoURL(address: string): string {
+  const overrideAddress = addressOverrides.get(address.toLowerCase()) || address
+  return getTokenLogoURLUni(overrideAddress)
+}
+
+export default function CurrencyLogo({
+  currency,
+  size = '24px',
+  style
+}: {
+  currency?: Currency
+  size?: string
+  style?: React.CSSProperties
+}) {
+  return <CurrencyLogoMod currency={currency} size={size} style={style} getTokenLogoURL={getTokenLogoURL} />
+}
