33a3f8fed9c7847b23af90e40869414b8d5325e9
biocom <michel@gnosis.pm>
2021-03-24 12:54:23 +0100

    Custom Hook

--- src/custom/pages/About/index.tsx
+++ src/custom/pages/About/index.tsx
@@ -5,12 +5,12 @@ import markdownContent from './about.md'
 import ReactMarkdown from 'react-markdown'
 
 export default function About() {
-  const content = useMarkdown(markdownContent)
-
   return (
     <AppBodyMod>
       <Title>About</Title>
-      <Content>{content.length > 0 && <ReactMarkdown>{content}</ReactMarkdown>}</Content>
+      <Content>
+        <ReactMarkdown>{useMarkdown(markdownContent)}</ReactMarkdown>
+      </Content>
     </AppBodyMod>
   )
 }
