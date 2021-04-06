c62d9e36b5fbc0e283ab23ae4f37d5065deff64d
biocom <michel@gnosis.pm>
2021-04-06 16:55:55 +0200

    Update logo/header component.

--- src/custom/components/Header/HeaderMod.tsx
+++ src/custom/components/Header/HeaderMod.tsx
@@ -41,7 +41,7 @@ const HeaderFrame = styled.div`
   width: 100%;
   top: 0;
   position: relative;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: ${({ theme }) => theme.header.border};
   padding: 1rem;
   z-index: 2;
   ${({ theme }) => theme.mediaWidth.upToMedium`
--- src/custom/components/Header/index.tsx
+++ src/custom/components/Header/index.tsx
@@ -41,22 +41,8 @@ export interface LinkType {
 
 export const HeaderModWrapper = styled(HeaderMod)`
   border-bottom: ${({ theme }) => theme.header.border};
-
-  ${UniIcon} {
-    display: flex;
-    margin: 0 16px 0 0;
-    position: relative;
-
-    &::after {
-      content: '${appStatus}';
-      display: block;
-      font-size: 10px;
-      font-weight: bold;
-      position: absolute;
-      right: 12px;
-      top: 2px;
-    }
-  }
+  background: red;
+  font-size: 200px;
 
   ${Title} {
     margin: 0;
@@ -81,18 +67,27 @@ export const LogoImage = styled.img.attrs(props => ({
   height: props.theme.logo.height
 }))`
   object-fit: contain;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    width: 150px;
+  `};
 `
-const AppStatusWrapper = styled.div`
+
+const UniIconMod = styled(UniIcon)`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 10px;
-  background: ${({ theme }) => theme.primary1};
-  border-radius: ${({ theme }) => theme.buttonPrimary.borderRadius};
-  /* negative margin matches logo margin right */
-  margin: auto 0 0 -10px;
-  padding: 2px 6px;
-  color: ${({ theme }) => theme.white};
+  margin: 0 16px 0 0;
+  position: relative;
+
+  &::after {
+    content: '${appStatus}';
+    display: block;
+    font-size: 10px;
+    font-weight: bold;
+    position: absolute;
+    right: 12px;
+    top: 2px;
+    color: ${({ theme }) => theme.text1};
+  }
 `
 
 export default function Header() {
@@ -104,11 +99,10 @@ export default function Header() {
     <HeaderModWrapper>
       <HeaderRow>
         <Title href=".">
-          <UniIcon>
+          <UniIconMod>
             <LogoImage />
-          </UniIcon>
+          </UniIconMod>
         </Title>
-        <AppStatusWrapper>{appStatus}</AppStatusWrapper>
         {/* <HeaderLinks>
           <StyledNavLink to="/swap">Swap</StyledNavLink>
           <StyledNavLink to="/about">About</StyledNavLink>
