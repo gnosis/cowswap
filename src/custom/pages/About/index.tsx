b6ae5576366f7de3be498bb5799a32c416790a88
David Sato <david.sato64@gmail.com>
2021-04-07 13:20:19 +0100

    use common Markdown comps

--- src/custom/pages/About/index.tsx
+++ src/custom/pages/About/index.tsx
@@ -1,16 +1,7 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import useFetchFile from '../../hooks/useFetchFile'
-import { Title, Content, AppBodyMod } from './About.styled'
-import content from './About.md'
+import content from 'pages/About/About.md'
+import Markdown from 'components/Markdown'
 
 export default function About() {
-  const { file, error } = useFetchFile(content)
-
-  return (
-    <AppBodyMod>
-      <Title>About</Title>
-      <Content>{file && <ReactMarkdown>{error ? error : file}</ReactMarkdown>}</Content>
-    </AppBodyMod>
-  )
+  return <Markdown content={content} title="About" />
 }
---
+++ src/custom/pages/PrivacyPolicy/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import content from './PrivacyPolicy.md'
+import Markdown from 'components/Markdown'
+
+export default function PrivacyPolicy() {
+  return <Markdown content={content} title="Privacy Policy" />
+}
