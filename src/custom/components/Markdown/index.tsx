48d616ca8657ae6ab1d8ff29e658ed79fde3bb07
David Sato <david.sato64@gmail.com>
2021-04-09 10:11:05 +0100

    Markdown: optional title and split file and error

--- src/custom/components/Markdown/index.tsx
+++ src/custom/components/Markdown/index.tsx
@@ -5,18 +5,23 @@ import { Title, Content, PageWrapper } from 'components/Markdown/markdown.styled
 
 interface MarkdownParams {
   content: string
-  title: ReactNode
+  title?: ReactNode
 }
 
 export default function Markdown({ content, title }: MarkdownParams) {
   const { error, file } = useFetchFile(content)
   return (
     <PageWrapper>
-      <Title>{title}</Title>
+      {title && <Title>{title}</Title>}
       <Content>
         {file && (
           <ReactMarkdown allowDangerousHtml linkTarget="_blank">
-            {error ? error : file}
+            {file}
+          </ReactMarkdown>
+        )}
+        {error && (
+          <ReactMarkdown allowDangerousHtml linkTarget="_blank">
+            {error}
           </ReactMarkdown>
         )}
       </Content>
