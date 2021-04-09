055ae969e414559c394ecae7edaf84766b9c203a
David Sato <david.sato64@gmail.com>
2021-04-09 13:34:24 +0100

    custom markdown comp

--- src/custom/components/Markdown/index.tsx
+++ src/custom/components/Markdown/index.tsx
@@ -1,12 +1,20 @@
 import React, { ReactNode } from 'react'
 import ReactMarkdown from 'react-markdown/with-html'
+import { ReactMarkdownPropsBase } from 'react-markdown'
 import useFetchFile from 'hooks/useFetchFile'
 import { Title, Content, PageWrapper } from 'components/Markdown/markdown.styled'
+import { HeadingRenderer, LinkRenderer } from './renderers'
 
 interface MarkdownParams {
   content: string
   title?: ReactNode
 }
+
+const CustomReactMarkdown = (props: ReactMarkdownPropsBase & { children: string }) => (
+  <ReactMarkdown {...props} renderers={{ heading: HeadingRenderer, link: LinkRenderer }} allowDangerousHtml>
+    {props.children}
+  </ReactMarkdown>
+)
 
 export default function Markdown({ content, title }: MarkdownParams) {
   const { error, file } = useFetchFile(content)
@@ -14,16 +22,8 @@ export default function Markdown({ content, title }: MarkdownParams) {
     <PageWrapper>
       {title && <Title>{title}</Title>}
       <Content>
-        {file && (
-          <ReactMarkdown allowDangerousHtml linkTarget="_blank">
-            {file}
-          </ReactMarkdown>
-        )}
-        {error && (
-          <ReactMarkdown allowDangerousHtml linkTarget="_blank">
-            {error}
-          </ReactMarkdown>
-        )}
+        {file && <CustomReactMarkdown>{file}</CustomReactMarkdown>}
+        {error && <CustomReactMarkdown>{error}</CustomReactMarkdown>}
       </Content>
     </PageWrapper>
   )
