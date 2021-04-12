c924f0e1dde9c2f8365d4a94d78f65393f922c28
David Sato <david.sato64@gmail.com>
2021-04-12 11:19:44 +0100

    optimise hash link scroll

--- src/custom/components/HashLink/index.tsx
+++ src/custom/components/HashLink/index.tsx
@@ -5,15 +5,17 @@ const HashLink = (props: LinkProps) => {
   const location = useLocation()
 
   useEffect(() => {
+    // bail out if the hash doesn't match our proposed anchor hash
+    if (props.to !== location.hash) return
+
     const id = location.hash.slice(1)
     const elem = document.getElementById(id)
 
-    if (!elem) return
+    // scroll element into view if it exists
+    elem?.scrollIntoView()
+  }, [location.hash, props.to])
 
-    window.scrollTo(0, elem.offsetTop)
-  }, [location.hash])
-
-  return <Link {...props}/>
+  return <Link {...props} />
 }
 
 export default HashLink
