a12b4b4bc0d3a7bd14f0e6fc9463e205abc4f2c4
David Sato <david.sato64@gmail.com>
2021-04-14 09:46:29 +0100

    remove scrollToTop

--- src/custom/components/Markdown/index.tsx
+++ src/custom/components/Markdown/index.tsx
@@ -4,7 +4,7 @@ import { ReactMarkdownPropsBase } from 'react-markdown'
 import useFetchFile from 'hooks/useFetchFile'
 import { Title, Content, PageWrapper } from 'components/Markdown/markdown.styled'
 import { HeadingRenderer, LinkRenderer } from './renderers'
-import ScrollToTop from '../ScrollToTop'
+// import ScrollToTop from '../ScrollToTop'
 
 interface MarkdownParams {
   content: string
@@ -24,13 +24,13 @@ export default function Markdown({ content, title }: MarkdownParams) {
         {file && <CustomReactMarkdown>{file}</CustomReactMarkdown>}
         {error && <CustomReactMarkdown>{error}</CustomReactMarkdown>}
       </Content>
-      <ScrollToTop
+      {/* <ScrollToTop
         styleProps={{
           bottom: '8.8%',
           right: 'calc(50% - 4.6rem)',
           background: '#9bd7c2'
         }}
-      />
+      /> */}
     </PageWrapper>
   )
 }
