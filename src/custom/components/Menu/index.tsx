07dfae4b77fec2fd918e7bbec2e5984f6551799f
Anxo Rodriguez <anxolin@gmail.com>
2021-04-14 15:50:39 +0200

    Add policies as links

--- src/custom/components/Menu/index.tsx
+++ src/custom/components/Menu/index.tsx
@@ -1,35 +1,53 @@
 import React from 'react'
-import { Code, MessageCircle, IconProps } from 'react-feather'
+import { Code, MessageCircle } from 'react-feather'
 
-import MenuMod, { MenuItem, InternalMenuItem } from './MenuMod'
+import MenuMod, { MenuItem, InternalMenuItem, MenuFlyout as MenuFlyoutUni } from './MenuMod'
 import styled from 'styled-components'
-import { FOOTER_URLS } from '../Footer'
 
 const CODE_LINK = 'https://github.com/gnosis/dex-swap'
-const DISCORD_LINK = 'https://chat.gnosis.io'
+const DISCORD_LINK = 'https://discord.gg/egGzDDctuC'
 
-export const StyledMenu = styled(MenuMod)``
+export const StyledMenu = styled(MenuMod)`
+  hr {
+    margin: 15px 0;
+  }
+`
 
-// remove static gnosis text and leave urls
-const CONTENT_URLS = FOOTER_URLS.slice(1) as { name: string; url: string; Icon?: React.FC<IconProps> }[]
+const Policy = styled(InternalMenuItem)`
+  font-size: 0.8em;
+  text-decoration: underline;
+`
+
+const MenuFlyout = styled(MenuFlyoutUni)`
+  min-width: 11rem;
+`
 
 export function Menu() {
   return (
     <StyledMenu>
-      <MenuItem id="link" href={CODE_LINK}>
-        <Code size={14} />
-        Code
-      </MenuItem>
-      <MenuItem id="link" href={DISCORD_LINK}>
-        <MessageCircle size={14} />
-        Discord
-      </MenuItem>
-      {CONTENT_URLS.map(({ name, url, Icon }, index) => (
-        <InternalMenuItem key={url + '_' + index} to={url}>
-          {Icon && <Icon size={14} />}
-          {name}
+      <MenuFlyout>
+        <InternalMenuItem to="/faq">
+          <Code size={14} />
+          FAQ
         </InternalMenuItem>
-      ))}
+
+        <MenuItem id="link" href={CODE_LINK}>
+          <Code size={14} />
+          Code
+        </MenuItem>
+        <MenuItem id="link" href={DISCORD_LINK}>
+          <MessageCircle size={14} />
+          Discord
+        </MenuItem>
+
+        <span>
+          <hr />
+        </span>
+
+        <Policy to="/terms-and-conditions">Terms and conditions</Policy>
+        <Policy to="/privacy-policy">Privacy policy</Policy>
+        <Policy to="/cookie-policy">Cookie policy</Policy>
+      </MenuFlyout>
     </StyledMenu>
   )
 }
