3ea55ee5dc4e780be5d91949b54290a42b764f73
Anxo Rodriguez <anxolin@gmail.com>
2021-04-14 20:43:59 +0200

    Customize wallet link

--- src/custom/connectors/index.ts
+++ src/custom/connectors/index.ts
@@ -1,16 +1,21 @@
 import { ChainId } from '@uniswap/sdk'
 import { InjectedConnector } from '@web3-react/injected-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
-export {
-  NETWORK_CHAIN_ID,
-  fortmatic,
-  portis,
-  network,
-  walletconnect,
-  walletlink,
-  getNetworkLibrary
-} from '@src/connectors'
+export { NETWORK_CHAIN_ID, fortmatic, portis, network, walletconnect, getNetworkLibrary } from '@src/connectors'
+
+const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
+if (typeof NETWORK_URL === 'undefined') {
+  throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
+}
 
 export const injected = new InjectedConnector({
   supportedChainIds: [ChainId.MAINNET, ChainId.ROPSTEN, ChainId.RINKEBY, ChainId.GÖRLI, ChainId.KOVAN, ChainId.XDAI]
+})
+
+// mainnet only
+export const walletlink = new WalletLinkConnector({
+  url: NETWORK_URL,
+  appName: 'Cow Swap',
+  appLogoUrl: 'https://raw.githubusercontent.com/gnosis/gp-swap-ui/develop/public/images/logo-square-512.png'
 })
