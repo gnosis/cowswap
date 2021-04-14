487cdd663fab48d184b4c61a436b29734d2d6176
David Sato <david.sato64@gmail.com>
2021-04-14 12:58:14 +0100

    Menu: give it content pages

--- src/custom/components/Menu/index.tsx
+++ src/custom/components/Menu/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Code, MessageCircle } from 'react-feather'
+import { Code, MessageCircle, IconProps } from 'react-feather'
 
-import MenuMod, { MenuItem } from './MenuMod'
+import MenuMod, { MenuItem, InternalMenuItem } from './MenuMod'
 import styled from 'styled-components'
+import { FOOTER_URLS } from '../Footer'
 
 const CODE_LINK = 'https://github.com/gnosis/dex-swap'
 const DISCORD_LINK = 'https://chat.gnosis.io'
 
 export const StyledMenu = styled(MenuMod)``
+
+// remove static gnosis text and leave urls
+const CONTENT_URLS = FOOTER_URLS.slice(1) as { name: string; url: string; Icon?: React.FC<IconProps> }[]
 
 export function Menu() {
   return (
@@ -20,6 +24,12 @@ export function Menu() {
         <MessageCircle size={14} />
         Discord
       </MenuItem>
+      {CONTENT_URLS.map(({ name, url, Icon }, index) => (
+        <InternalMenuItem key={url + '_' + index} to={url}>
+          {Icon && <Icon size={14} />}
+          {name}
+        </InternalMenuItem>
+      ))}
     </StyledMenu>
   )
 }
