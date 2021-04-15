6be772b7ac1328d1def0466ce1b5c2088d444200
Anxo Rodriguez <anxolin@gmail.com>
2021-04-15 19:38:36 +0200

    Make regex case-insensitive

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -17,7 +17,7 @@ export * from '@src/constants/index'
 
 export const PRODUCTION_URL = 'cow.trade'
 
-const DISABLED_WALLETS = /^(?:WALLET_LINK|COINBASE_LINK|WALLET_CONNECT|FORTMATIC|Portis)$/
+const DISABLED_WALLETS = /^(?:WALLET_LINK|COINBASE_LINK|WALLET_CONNECT|FORTMATIC|Portis)$/i
 
 // Re-export only the supported wallets
 export const SUPPORTED_WALLETS = Object.keys(SUPPORTED_WALLETS_UNISWAP).reduce((acc, key) => {
