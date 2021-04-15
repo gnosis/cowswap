dcb809f33f58937fc8d84dde1cec2a18e6c1ccd0
David <david.sato64@gmail.com>
2021-04-15 15:07:37 +0100

    263.2 - Support internal hash links (markdown related) (#398)
    * update internal links
    
    * custom markdown comp
    
    * HashLink creation
    
    * create renderers
    
    * scrollToTop component
    
    * remove explicit props.children
    
    Co-authored-by: Velenir <Velenir@users.noreply.github.com>
    
    * make optoinal scss more concise
    
    Co-authored-by: Velenir <Velenir@users.noreply.github.com>
    
    * RegExp.test
    
    Co-authored-by: Velenir <Velenir@users.noreply.github.com>
    
    * dont need explicit children props passing thanks @velenir
    
    * optimise renderer (header)
    
    thanks @velenir
    
    * optimised state/logic in ScrollToTop
    
    thanks @velenir
    
    * optimise hash link scroll
    
    * remove scrollToTop
    
    * remove commented code
    
    * fix url
    
    * 250[part of it] Add Terms and Conditions page (#409)
    
    * add about.md title
    
    1. remove title from markdown wrapper
    
    * add TOCs md and comp
    
    * add TOCs to app
    
    * terms formatting
    
    * [250] Footer Container and better CSS formatting (#424)
    
    * remove versions from appbody wrapper
    
    * create Footer
    
    * add Footer to AppMod
    
    * edit Version scss
    
    * css
    
    * border on large screens
    
    * keep footer relatve
    
    * key in footer
    
    * [250] Header Menu content pages ([...] Menu) (#425)
    
    * FooterUrl icons
    
    * re-enable HeaderElementWrap
    
    * Header: gets menu back
    
    * Menu: give it content pages
    
    * InternalMenuItem comp
    
    * Add policies as links (#427)
    
    * Add policies as links
    
    * Fix link
    
    * use separator
    
    * fix import
    
    Co-authored-by: David Sato <david.sato64@gmail.com>
    
    Co-authored-by: Anxo Rodriguez <anxolin@gmail.com>
    
    * remove footer links (#439)
    
    Co-authored-by: Anxo Rodriguez <anxolin@gmail.com>
    
    Co-authored-by: Velenir <Velenir@users.noreply.github.com>
    Co-authored-by: Anxo Rodriguez <anxolin@gmail.com>

--- src/custom/components/Markdown/index.tsx
+++ src/custom/components/Markdown/index.tsx
@@ -1,12 +1,19 @@
 import React, { ReactNode } from 'react'
 import ReactMarkdown from 'react-markdown/with-html'
+import { ReactMarkdownPropsBase } from 'react-markdown'
 import useFetchFile from 'hooks/useFetchFile'
 import { Title, Content, PageWrapper } from 'components/Markdown/markdown.styled'
+import { HeadingRenderer, LinkRenderer } from './renderers'
+// import ScrollToTop from '../ScrollToTop'
 
 interface MarkdownParams {
   content: string
   title?: ReactNode
 }
+
+const CustomReactMarkdown = (props: ReactMarkdownPropsBase & { children: string }) => (
+  <ReactMarkdown {...props} renderers={{ heading: HeadingRenderer, link: LinkRenderer }} allowDangerousHtml />
+)
 
 export default function Markdown({ content, title }: MarkdownParams) {
   const { error, file } = useFetchFile(content)
@@ -14,17 +21,16 @@ export default function Markdown({ content, title }: MarkdownParams) {
     <PageWrapper>
       {title && <Title>{title}</Title>}
       <Content>
-        {file && (
-          <ReactMarkdown allowDangerousHtml linkTarget="_blank">
-            {file}
-          </ReactMarkdown>
-        )}
-        {error && (
-          <ReactMarkdown allowDangerousHtml linkTarget="_blank">
-            {error}
-          </ReactMarkdown>
-        )}
+        {file && <CustomReactMarkdown>{file}</CustomReactMarkdown>}
+        {error && <CustomReactMarkdown>{error}</CustomReactMarkdown>}
       </Content>
+      {/* <ScrollToTop
+        styleProps={{
+          bottom: '8.8%',
+          right: 'calc(50% - 4.6rem)',
+          background: '#9bd7c2'
+        }}
+      /> */}
     </PageWrapper>
   )
 }
