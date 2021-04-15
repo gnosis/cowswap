44db9fe7df3d701dc019278c526aba193d59ffe3
David Sato <david.sato64@gmail.com>
2021-04-15 11:58:03 +0100

    remove footer links

--- src/custom/components/Footer/index.tsx
+++ src/custom/components/Footer/index.tsx
@@ -1,20 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 import Version from '../Version'
-import { BookOpen, Search, Book, AlertTriangle } from 'react-feather'
-export const FOOTER_URLS = [
-  { name: '© 2021 Gnosis' },
-  {
-    name: 'About',
-    url: '/about',
-    Icon: BookOpen
-  },
-  { name: 'Cookies', url: '/cookie-policy', Icon: Search },
-  { name: 'Privacy', url: '/privacy-policy', Icon: AlertTriangle },
-  { name: 'Terms', url: '/terms-and-conditions', Icon: Book }
-]
 
 const FooterVersion = styled(Version)`
   margin-right: auto;
@@ -34,44 +21,11 @@ const Wrapper = styled.div`
   `}
 `
 
-const UrlWrapper = styled(Wrapper)`
-  align-self: flex-end;
-  margin: 0 0 1rem 0;
-
-  > * {
-    color: black;
-    font-size: small;
-    text-align: center;
-    white-space: nowrap;
-    margin: 0 0.5rem;
-    width: 20%;
-
-    &:not(:last-child) {
-      border-right: 1px solid #00000040;
-
-      ${({ theme }) => theme.mediaWidth.upToLarge`
-        border: none;
-      `}
-    }
-  }
-`
-
-export default function Footer() {
+export default function Footer({ children }: { children?: React.ReactChildren }) {
   return (
     <Wrapper>
       <FooterVersion />
-      <UrlWrapper>
-        {FOOTER_URLS.map(({ name, url }, i) => {
-          const key = url || name + '_' + i
-          return url ? (
-            <Link key={key} to={url}>
-              {name}
-            </Link>
-          ) : (
-            <span key={key}>{name}</span>
-          )
-        })}
-      </UrlWrapper>
+      {children}
     </Wrapper>
   )
 }
