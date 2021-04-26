7742f2464368b6872c3e99354a52a94e045b3bfd
biocom <michel@gnosis.pm>
2021-04-26 15:16:53 +0200

    Styling of the feedback button.

--- src/custom/components/AppziButton/index.tsx
+++ src/custom/components/AppziButton/index.tsx
@@ -1,11 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
+import { ButtonPrimary } from '../Button'
+import FeedbackIcon from './../../assets/cow-swap/feedback.svg'
 
-const Wrapper = styled.button`
-  position: absolute;
-  bottom: 40px;
-  right: 20px;
-  font-size: 40pt;
+const Wrapper = styled(ButtonPrimary)`
+  border-radius: 46px;
+  font-size: 100%;
+  height: 46px;
+  width: 46px;
+  padding: 0;
+  margin: 0;
+  z-index: 0;
+  position: fixed;
+  bottom: 60px;
+  right: 16px;
+  overflow: visible;
+  z-index: 2;
+
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+    bottom: 86px;
+  `};
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    right: 14px;
+  `};
+
+  &::after {
+    content: 'Give feedback!';
+    display: block;
+    position: absolute;
+    top: 0;
+    color: ${({ theme }) => theme.black};
+    background: ${({ theme }) => theme.primary1};
+    padding: 9px;
+    font-size: 12px;
+    border-radius: 14px;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out, visibility 0.2s ease-in-out, top 0.2s ease-in-out;
+  }
+
+  &:hover::after {
+    top: -60px;
+    visibility: visible;
+    opacity: 1;
+  }
+
+  > img {
+    width: 100%;
+    height: 100%;
+    padding: 8px;
+    object-fit: contain;
+  }
 `
 const APPZI_KEY = 'f7591eca-72f7-4888-b15f-e7ff5fcd60cd'
 
@@ -22,5 +68,9 @@ function openWidget() {
 }
 
 export default function Appzi() {
-  return <Wrapper onClick={openWidget}>🐮</Wrapper>
+  return (
+    <Wrapper onClick={openWidget}>
+      <img src={FeedbackIcon} alt="Provide Feedback" />
+    </Wrapper>
+  )
 }
