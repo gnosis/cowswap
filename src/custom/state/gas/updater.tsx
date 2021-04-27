f062ac115fe7cf0a33e804a2f39a14310c4a39ad
David Sato <david.sato64@gmail.com>
2021-04-27 15:00:37 +0100

    updater logic minor edit

--- src/custom/state/gas/updater.tsx
+++ src/custom/state/gas/updater.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
-import { useGasPrices, useUpdateGasPrices, getGasPrices } from './hooks'
+import { useGasPrices, useUpdateGasPrices } from './hooks'
 import { useActiveWeb3React } from 'hooks'
 import { GAS_PRICE_UPDATE_THRESHOLD } from 'constants/index'
+import { fetchGasPrices } from 'utils/gas'
 
-function needsGasUpdate(now: number, lastUpdated: number, threshold: number) {
+function shouldUpdateGasPrices(now: number, lastUpdated: number, threshold: number) {
   return now - lastUpdated > threshold
 }
 
@@ -14,12 +15,12 @@ export default function GasUpdater(): null {
 
   useEffect(() => {
     const now = Date.now()
-    const updated = gas ? Date.parse(gas.lastUpdate) : null
+    const updated = gas?.lastUpdate ? Date.parse(gas.lastUpdate) : null
 
     // if no gas in local/redux state OR time threshold has passed
     // since last update, then:
-    if (!updated || needsGasUpdate(now, updated, GAS_PRICE_UPDATE_THRESHOLD)) {
-      getGasPrices(chainId)
+    if (!updated || shouldUpdateGasPrices(now, updated, GAS_PRICE_UPDATE_THRESHOLD)) {
+      fetchGasPrices(chainId)
         .then(gas => {
           updateGasPrices({
             ...gas,
