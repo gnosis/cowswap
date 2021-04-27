2f3d58c2a2f9cb1baa946305aa1c1aa6fe4ccc8b
biocom <michel@gnosis.pm>
2021-04-27 15:32:56 +0200

    Make button colors consistent.

--- src/custom/components/CurrencyInputPanel/CurrencyInputPanelMod.tsx
+++ src/custom/components/CurrencyInputPanel/CurrencyInputPanelMod.tsx
@@ -24,22 +24,23 @@ export const InputRow = styled.div<{ selected: boolean }>`
 
 export const CurrencySelect = styled.button<{ selected: boolean }>`
   align-items: center;
-  height: 2.2rem;
+  height: 36px;
   font-size: 20px;
   font-weight: 500;
   background-color: ${({ selected, theme }) => (selected ? theme.bg1 : theme.primary1)};
-  color: ${({ selected, theme }) => (selected ? theme.text1 : theme.white)};
+  color: ${({ theme }) => theme.black};
   border-radius: 12px;
   box-shadow: ${({ selected }) => (selected ? 'none' : '0px 6px 10px rgba(0, 0, 0, 0.075)')};
   outline: none;
   cursor: pointer;
   user-select: none;
   border: none;
-  padding: 0 0.5rem;
+  padding: 0 6px;
+  transition: background-color 0.2s ease-in-out;
 
   :focus,
   :hover {
-    background-color: ${({ selected, theme }) => (selected ? theme.bg2 : darken(0.05, theme.primary1))};
+    background-color: ${({ theme }) => darken(0.05, theme.primary1)};
   }
 `
 
@@ -67,7 +68,7 @@ export const StyledDropDown = styled(DropDown)<{ selected: boolean }>`
   height: 35%;
 
   path {
-    stroke: ${({ selected, theme }) => (selected ? theme.text1 : theme.white)};
+    stroke: ${({ selected, theme }) => (selected ? theme.black : theme.black)};
     stroke-width: 1.5px;
   }
 `
