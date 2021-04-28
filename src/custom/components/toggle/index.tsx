cadddebe5abae23df5a7a1f78b6afb4dbe6c2421
Anxo Rodriguez <anxolin@gmail.com>
2021-04-28 18:17:07 +0200

    Disable off button style

--- src/custom/components/toggle/index.tsx
+++ src/custom/components/toggle/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import ToggleUni, { ToggleProps } from '@src/components/Toggle'
+import ToggleUni, { ToggleProps as TogglePropsUni } from '@src/components/Toggle'
 
-export * from '@src/components/Toggle'
+export type ToggleProps = TogglePropsUni
 
 const Wrapper = styled(ToggleUni)`
   .disabled {
