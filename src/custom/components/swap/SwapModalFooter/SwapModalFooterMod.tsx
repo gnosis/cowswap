885a485a892cb5b064dc1a1be89cecf3e4ad4224
Anxo Rodriguez <anxolin@gmail.com>
2021-05-10 17:54:49 +0200

    Fix compiling issues

--- src/custom/components/swap/SwapModalFooter/SwapModalFooterMod.tsx
+++ src/custom/components/swap/SwapModalFooter/SwapModalFooterMod.tsx
@@ -1,4 +1,4 @@
-import { CurrencyAmount, Percent, /* Trade, */ TradeType } from '@uniswap/sdk'
+import { CurrencyAmount, /* Percent,  Trade, */ TradeType } from '@uniswap/sdk'
 import React, { useContext, useMemo, useState } from 'react'
 import { Repeat } from 'react-feather'
 import { Text } from 'rebass'
