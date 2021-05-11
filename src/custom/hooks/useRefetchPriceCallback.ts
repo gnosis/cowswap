aca7c0ff2a227aadbda5a274c0ca1a5bb58ddf68
David W3stside <david.sato64@gmail.com>
2021-05-12 00:30:52 +0100

    check return type for new error

--- src/custom/hooks/useRefetchPriceCallback.ts
+++ src/custom/hooks/useRefetchPriceCallback.ts
@@ -1,9 +1,8 @@
 import { useCallback } from 'react'
 import { useClearQuote, useUpdateQuote } from 'state/price/hooks'
 import { getCanonicalMarket, registerOnWindow } from 'utils/misc'
-import { FeeQuoteParams, getFeeQuote, getPriceQuote } from 'utils/operator'
-import { UnsupportedToken } from 'state/price/reducer'
-import { isFeeOrPriceInformation, isUnsupportedToken } from 'state/price/utils'
+import { ErrorHandlingResponse, FeeQuoteParams, getFeeQuote, getPriceQuote } from 'utils/operator'
+import { PriceInformation, UnsupportedToken } from 'state/price/reducer'
 import { useAddUnsupportedToken, useDynamicallyUnsupportedTokens } from 'state/unsupportedTokens/hooks'
 
 export interface RefetchQuoteCallbackParmams {
@@ -14,6 +13,13 @@ export interface RefetchQuoteCallbackParmams {
 function handleError(err: any): undefined {
   console.error('Error fetching price/fee', err)
   return undefined
+}
+
+// quote endpoint can return error we want to consume
+const _validResponse = <T, E = UnsupportedToken>(response?: ErrorHandlingResponse<T, E>) => {
+  if (!response || response.error) return false
+
+  return Boolean(response.data)
 }
 
 /**
@@ -43,23 +49,24 @@ export function useRefetchQuoteCallback() {
       // use typecheck on returned fee/price response
       // can be a valid FeeInformation/Price object
       // or an UnsupportedToken error response
-      if (isFeeOrPriceInformation(fee) && isFeeOrPriceInformation(price)) {
+      if ((_validResponse(fee) || !fetchFee) && _validResponse(price)) {
         // Update quote
+        // fee.data can still be undefined
+        // price.data CANNOT be undefined (L22)
         // TODO: check this
-        if ((fee || !fetchFee) && price) {
-          updateQuote({
-            sellToken,
-            buyToken,
-            amount,
-            price,
-            chainId,
-            lastCheck: Date.now(),
-            // Fee is only updated when fetchFee=true
-            fee
-          })
-        }
-      } else if (isUnsupportedToken(price) || isUnsupportedToken(fee)) {
-        const unsupportedToken = isUnsupportedToken(price) ? price : (fee as UnsupportedToken)
+        updateQuote({
+          sellToken,
+          buyToken,
+          amount,
+          // TS hates the typecheck on L52, price.data is not undefined
+          price: price?.data as PriceInformation,
+          chainId,
+          lastCheck: Date.now(),
+          // Fee is only updated when fetchFee=true
+          fee: fee?.data
+        })
+      } else if ((price && !_validResponse(price)) || (fee && !_validResponse(fee))) {
+        const unsupportedToken = (price?.error || fee?.error) as UnsupportedToken
         // unsupported token error, mark token as such
         // TODO: will likely change with server response including a data.address
         // field for UnsupportedToken error responses
