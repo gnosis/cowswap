efdd87a2f942ce2c54d219417f11101ffd9ddd69
David W3stside <david.sato64@gmail.com>
2021-05-13 17:13:32 +0100

    better comments

--- src/custom/state/swap/trade.test.ts
+++ src/custom/state/swap/trade.test.ts
@@ -1,8 +1,16 @@
 import { parseUnits } from '@ethersproject/units'
 import { getCanonicalMarket } from '@src/custom/utils/misc'
-import { getPriceQuote } from '@src/custom/utils/operator'
+import { PriceQuoteParams } from '@src/custom/utils/operator'
 import { basisPointsToPercent } from '@src/utils'
 import { ChainId, Fraction, JSBI, Pair, Route, Token, TokenAmount, Trade, TradeType, WETH } from '@uniswap/sdk'
+
+async function mockGetPriceQuote(params: PriceQuoteParams & { mockedPriceOut: string }) {
+  const { quoteToken, mockedPriceOut } = params
+  return Promise.resolve({
+    amount: mockedPriceOut,
+    token: quoteToken
+  })
+}
 
 const WETH_MAINNET = new Token(ChainId.MAINNET, WETH[1].address, 18)
 const DAI_MAINNET = new Token(ChainId.MAINNET, '0x6b175474e89094c44da98b954eedeac495271d0f', 18)
@@ -20,6 +28,8 @@ describe('Swap PRICE Quote test', () => {
     const amountIn = parseUnits('1').toString()
     const currencyIn = new TokenAmount(WETH_MAINNET, amountIn)
 
+    const MOCKED_PRICE_OUT = parseUnits('4273', DAI_MAINNET.decimals).toString()
+
     beforeEach(async () => {
       // make a new Trade object
       inTrade = new Trade(new Route([PAIR_WETH_DAI], WETH_MAINNET), currencyIn, TradeType.EXACT_INPUT)
@@ -31,7 +41,10 @@ describe('Swap PRICE Quote test', () => {
       })
 
       // get the WETH/DAI quote
-      quote = await getPriceQuote({
+      // mock and return MOCKED_PRICE_OUT
+      quote = await mockGetPriceQuote({
+        // custom amount out we want as part of mock
+        mockedPriceOut: MOCKED_PRICE_OUT,
         chainId: 1,
         quoteToken,
         baseToken,
@@ -47,7 +60,7 @@ describe('Swap PRICE Quote test', () => {
       }
     })
 
-    it('Expect FAIL: ExactIN swap: 0.5% Slippage', async () => {
+    it('[Expect FAIL] ExactIN swap: 0.5% Slippage: <outputAmount * (1 - 0.005)>', async () => {
       // we expect a slippage of (1 - slippage) e.g 1 - 0.005 = 0.995
       const expectedSlippage = new Fraction('995', '1000')
       // let's check against the way Uni calculates it exactly using 0.5% slippage
@@ -78,19 +91,22 @@ describe('Swap PRICE Quote test', () => {
       expect(actualOutput).not.toEqual(expectedOutput.quotient.toString())
     })
 
-    it('Expect PASS: ExactIN swap: 0.5% Slippage', async () => {
+    it('[Expect PASS] ExactIN swap: 0.5% Slippage: <outputAmount * (1 / (1 + 0.005))>', async () => {
       // calculate slippage EXACTLY as uni does
       // first we convert basis points slippage 50 (0.005) to Percent
       // then we add to Fraction(1) and INVERT
       const slippagePercent = basisPointsToPercent(50)
+      // NOTE: the .invert() here, as Uni does it, doesn't return a clean
+      // amount as we might expect, hence the issue.
+      // it can be rewritten as: (1 / (1 + 0.005<slippage>)) = 0.9950248756218907
       const expectedSlippage = new Fraction('1').add(slippagePercent).invert()
 
       // multiply our quoted amount by the expectedSlippage
       const expectedOutput = new Fraction(quote.amount).multiply(expectedSlippage)
       // Actual:
-      const actualSlippage = extendedTradeIn.minimumAmountOut(slippagePercent).raw.toString()
+      const actualOutput = extendedTradeIn.minimumAmountOut(slippagePercent).raw.toString()
 
-      expect(actualSlippage).toEqual(expectedOutput.quotient.toString())
+      expect(actualOutput).toEqual(expectedOutput.quotient.toString())
     })
   })
 })
