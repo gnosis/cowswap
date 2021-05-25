bff610d3933f691fc197f12eadbb8eac676391da
Anxo Rodriguez <anxolin@gmail.com>
2021-05-25 20:18:43 +0200

    Add menu item

--- src/custom/components/Menu/index.tsx
+++ src/custom/components/Menu/index.tsx
@@ -1,15 +1,45 @@
 import React from 'react'
 import { Code, MessageCircle /*PieChart*/ } from 'react-feather'
 
-import MenuMod, { MenuItem, InternalMenuItem, MenuFlyout as MenuFlyoutUni } from './MenuMod'
-import { useCloseModals } from 'state/application/hooks'
+import MenuMod, { MenuItem, InternalMenuItem, MenuFlyout as MenuFlyoutUni, StyledMenuButton } from './MenuMod'
+import { useCloseModals, useToggleModal } from 'state/application/hooks'
 import styled from 'styled-components'
 import { Separator as SeparatorBase } from 'components/swap/styleds'
 import { CONTRACTS_CODE_LINK, DISCORD_LINK } from 'constants/index'
 
+import { ApplicationModal } from '@src/state/application/actions'
+
 export const StyledMenu = styled(MenuMod)`
   hr {
     margin: 15px 0;
+  }
+`
+
+export const MenuButton = styled.button`
+  color: ${({ theme }) => theme.text2};
+  background: none;
+  border: none;
+  font-size: 1rem;
+  text-align: left;
+
+  font-weight: 500;
+
+  :focus {
+    outline: none;
+    text-decoration: underline;
+  }
+
+  :active {
+    text-decoration: none;
+  }
+
+  :hover {
+    text-decoration: underline;
+    color: ${({ theme }) => theme.text1};
+    cursor: pointer;
+    text-decoration: none;
+    background: #afcbda;
+    border-radius: 6px;
   }
 `
 
@@ -124,6 +154,7 @@ export const CloseMenu = styled.button`
 
 export function Menu() {
   const close = useCloseModals()
+  const openCowGameModal = useToggleModal(ApplicationModal.COW_GAME)
 
   return (
     <StyledMenu>
@@ -133,12 +164,10 @@ export function Menu() {
           <Code size={14} />
           FAQ
         </InternalMenuItem>
-
         {/* <MenuItem id="link" href={DUNE_DASHBOARD_LINK}>
           <PieChart size={14} />
           Stats
         </MenuItem> */}
-
         <MenuItem id="link" href={CONTRACTS_CODE_LINK}>
           <span onClick={close}>
             <Code size={14} />
@@ -152,8 +181,11 @@ export function Menu() {
           </span>
         </MenuItem>
 
-        <Separator />
+        <MenuButton id="link" onClick={openCowGameModal}>
+          <span onClick={close}>🐮 Game</span>
+        </MenuButton>
 
+        <Separator />
         <Policy to="/terms-and-conditions" onClick={close}>
           Terms and conditions
         </Policy>
