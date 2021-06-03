5e1241812afe4cf154d4f25ca097af4ede865d02
David W3stside <david.sato64@gmail.com>
2021-06-03 10:44:14 +0100

    TradePriceMod

---
+++ src/custom/components/swap/TradePrice/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import useLoadingWithTimeout from 'hooks/useLoadingWithTimeout'
+import { useIsQuoteSoftLoading } from 'state/price/hooks'
+import TradePriceModComponent, { TradePriceProps } from './TradePriceMod'
+import { AnimatedImg } from 'pages/Swap'
+import loadingCowGif from 'assets/cow-swap/cow-load.gif'
+import repeat from 'assets/svg/repeat.svg'
+import { StyledBalanceMaxMini } from 'components/swap/styleds'
+
+const TradePriceMod = styled(TradePriceModComponent)`
+  ${StyledBalanceMaxMini} {
+    width: 30px;
+    height: 30px;
+    position: relative;
+    overflow: hidden;
+  }
+`
+
+const LoadingCowComponent = ({ showCow }: { showCow: boolean }) => (
+  <>
+    <AnimatedImg src={loadingCowGif} showLoader={showCow} />
+    <AnimatedImg src={repeat} showLoader={!showCow} width="50px" />
+  </>
+)
+
+export default function TradePrice(props: TradePriceProps) {
+  const isQuoteSoftLoading = useIsQuoteSoftLoading()
+  const showCow = useLoadingWithTimeout(isQuoteSoftLoading, 4000)
+
+  return <TradePriceMod {...props} LoadingCow={<LoadingCowComponent showCow={showCow} />} />
+}
