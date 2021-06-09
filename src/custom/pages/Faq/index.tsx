3135814b04178245755d2b2997b69fcc0ce68640
Alex V <80408352+avsavsavs@users.noreply.github.com>
2021-06-09 10:57:31 +0200

    Incldue one new FAQ Questiion under "General"
    Included the question "Why is Cowswap a META DEX Aggregator?". The question goes from line 176 to 183

--- src/custom/pages/Faq/index.tsx
+++ src/custom/pages/Faq/index.tsx
@@ -173,6 +173,15 @@ export default function Faq() {
             features while placing gas free trades.
           </p>
 
+          <h3 id="why-is-cowswap-a-meta-dex-aggregator">Why is CowSwap a Meta DEX aggregator?</h3>
+          <p>
+            Cowswap is built on top of the Gnosis protocol which matches trades via batch auctions on a variety of on-chain liquidity.
+            Trades can be settled via underlying on-chain AMMs directly or via DEX Aggregators, depending on which pool /path offers 
+            the best price. It is thus essentially acting as a DexAggregator of the DexAggregators.  In addition to that, before 
+            finding the best trading price for a trade from the available on-chain liquidity, Gnosis Protocol first checks if it 
+            can find a coincidence of wants within the existing batch to offer an even better price than any pool can.
+          </p>          
+          
           <h3 id="what-is-mev-and-how-much-mev-has-been-extracted-from-users-to-date">
             What is MEV and how much MEV has been extracted from users to date?
           </h3>
