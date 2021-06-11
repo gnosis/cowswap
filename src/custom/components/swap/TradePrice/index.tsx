936d94072ba133e6e72bddcfa4a0dc7748bcdd5d
biocom <michel@gnosis.pm>
2021-06-11 14:37:50 +0200

    Address comments.

--- src/custom/components/swap/TradePrice/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-import TradePriceModComponent, { TradePriceProps } from './TradePriceMod'
-
-const TradePriceMod = styled(TradePriceModComponent)``
-
-export default function TradePrice(props: TradePriceProps) {
-  return <TradePriceMod {...props} />
-}
