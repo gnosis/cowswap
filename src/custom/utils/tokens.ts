780a16323d572de93f5fcd9ed21990f326d19582
Anxo Rodriguez <anxolin@gmail.com>
2021-06-22 16:46:03 +0200

    Fix issue with selling ETH

--- src/custom/utils/tokens.ts
+++ src/custom/utils/tokens.ts
@@ -2,7 +2,7 @@ import { ChainId, ETHER, WETH } from '@uniswap/sdk'
 
 export function toErc20Address(tokenAddress: string, chainId: ChainId): string {
   let checkedAddress = tokenAddress
-  if (tokenAddress === ETHER.symbol) {
+  if (tokenAddress === 'ETH' || tokenAddress === ETHER.symbol) {
     checkedAddress = WETH[chainId].address
   }
 
