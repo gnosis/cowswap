7a0e5e423bb8ed329c998b29fcb85c967bdffb2f
David W3stside <david.sato64@gmail.com>
2021-06-25 12:17:10 +0100

    supportedChainId mod

---
+++ src/custom/utils/supportedChainId.ts
@@ -0,0 +1,13 @@
+import { SupportedChainId } from '../constants/chains'
+
+/**
+ * Returns the input chain ID if chain is supported. If not, return undefined
+ * @param chainId a chain ID, which will be returned if it is a supported chain ID
+ */
+//  export function supportedChainId(chainId: number): number | undefined {
+export function supportedChainId(chainId: number): SupportedChainId | undefined {
+  if (chainId in SupportedChainId) {
+    return chainId
+  }
+  return undefined
+}
