48cd627578f9160e22225be92266dc6a593a345e
Anxo Rodriguez <anxolin@gmail.com>
2021-06-29 14:03:53 +0200

    Fix issue negative amont

--- src/custom/pages/Swap/SwapMod.tsx
+++ src/custom/pages/Swap/SwapMod.tsx
@@ -374,6 +374,9 @@ export default function Swap({
   )
 
   const swapBlankState = !swapInputError && !trade
+  const amountBeforeFees = trade?.inputAmountWithFee.lessThan(trade.fee.amount)
+    ? trade?.inputAmountWithFee.subtract(trade.fee.feeAsCurrency).toSignificant(DEFAULT_PRECISION)
+    : '0'
 
   return (
     <>
@@ -410,9 +413,7 @@ export default function Swap({
                   label={exactInLabel}
                   trade={trade}
                   showHelper={independentField === Field.OUTPUT}
-                  amountBeforeFees={trade?.inputAmountWithFee
-                    .subtract(trade.fee.feeAsCurrency)
-                    .toSignificant(DEFAULT_PRECISION)}
+                  amountBeforeFees={amountBeforeFees}
                   amountAfterFees={trade?.inputAmountWithFee.toSignificant(DEFAULT_PRECISION)}
                   type="From"
                   feeAmount={trade?.fee?.feeAsCurrency?.toSignificant(DEFAULT_PRECISION)}
