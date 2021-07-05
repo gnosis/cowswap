57463aee9d5c9173015aa8d6000b41a92f318785
David W3stside <david.sato64@gmail.com>
2021-07-05 15:28:52 +0100

    fix confirmation modal

--- src/custom/components/swap/SwapModalFooter/index.tsx
+++ src/custom/components/swap/SwapModalFooter/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { computeTradePriceBreakdown, FEE_TOOLTIP_MSG } from '../TradeSummary/TradeSummaryMod'
 import SwapModalFooterMod, { SwapModalFooterProps } from './SwapModalFooterMod'
 import { StyledBalanceMaxMini } from 'components/swap/styleds'
 import { RowBetween, RowFixed } from 'components/Row'
@@ -17,23 +16,10 @@ const Wrapper = styled.div`
   }
 `
 
-export default function SwapModalFooter(props: Omit<SwapModalFooterProps, 'fee' | 'priceImpactWithoutFee'>) {
-  const { /*priceImpactWithoutFee,*/ realizedFee } = React.useMemo(
-    () => computeTradePriceBreakdown(props.trade),
-    [props.trade]
-  )
-
+export default function SwapModalFooter(props: SwapModalFooterProps) {
   return (
     <Wrapper>
-      <SwapModalFooterMod
-        {...props}
-        fee={{
-          feeTitle: 'Fee',
-          feeAmount: realizedFee,
-          feeTooltip: FEE_TOOLTIP_MSG,
-        }}
-        // priceImpactWithoutFee={priceImpactWithoutFee}
-      />
+      <SwapModalFooterMod {...props} />
     </Wrapper>
   )
 }
