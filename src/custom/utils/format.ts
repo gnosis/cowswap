a803278a82e9140e24415eda8c433947e9a9dd7b
David W3stside <david.sato64@gmail.com>
2021-07-07 20:21:11 +0100

    simplify formatSmart for cowswap

--- src/custom/utils/format.ts
+++ src/custom/utils/format.ts
@@ -1,9 +1,20 @@
 import BigNumber from 'bignumber.js'
 
-export * from '@gnosis.pm/dex-js'
+import { formatSmart as _formatSmart } from '@gnosis.pm/dex-js'
+import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 
 const TEN = new BigNumber(10)
 
 export function formatAtoms(amount: string, decimals: number): string {
   return new BigNumber(amount).div(TEN.pow(decimals)).toString(10)
+}
+
+export function formatSmart(currency: CurrencyAmount<Currency> | undefined, precision: number) {
+  if (!currency) return
+
+  return _formatSmart({
+    amount: currency.quotient.toString(),
+    precision: currency.currency.decimals,
+    decimals: precision,
+  })
 }
