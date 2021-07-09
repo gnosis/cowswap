d6803cd69521d300800c00507dd2e02d0a70cdc3
David W3stside <david.sato64@gmail.com>
2021-07-09 11:44:26 +0100

    re-add priceImpact to input

--- src/custom/pages/Swap/SwapMod.tsx
+++ src/custom/pages/Swap/SwapMod.tsx
@@ -540,7 +540,7 @@ export default function Swap({
                 showMaxButton={false}
                 hideBalance={false}
                 fiatValue={fiatValueOutput ?? undefined}
-                // priceImpact={priceImpact}
+                priceImpact={priceImpact}
                 currency={currencies[Field.OUTPUT]}
                 onCurrencySelect={handleOutputSelect}
                 otherCurrency={currencies[Field.INPUT]}
