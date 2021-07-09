8c47548311b7a01a97ea765228d515012c327348
David W3stside <david.sato64@gmail.com>
2021-07-09 11:19:31 +0100

    use percent_precision in tooltips

--- src/custom/utils/tooltips.ts
+++ src/custom/utils/tooltips.ts
@@ -1,4 +1,5 @@
 import { Percent } from '@uniswap/sdk-core'
+import { PERCENTAGE_PRECISION } from '../constants'
 import { formatSmart } from './format'
 
 export function getMinimumReceivedTooltip(allowedSlippage: Percent, isExactIn: boolean): string {
@@ -8,6 +9,6 @@ export function getMinimumReceivedTooltip(allowedSlippage: Percent, isExactIn: b
     isExactIn ? "Minimum tokens you'll receive." : "Maximum tokens you'll sell."
   } This accounts for the current price and your chosen slippage (${formatSmart(
     slippagePercent,
-    2
+    PERCENTAGE_PRECISION
   )}%). If the price moves beyond your slippage, you won't trade but also you won't pay any fees or gas.`
 }
