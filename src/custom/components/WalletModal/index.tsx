953ef536bef4b3bd211df76899b1d4efb52f929d
biocom <michel@gnosis.pm>
2021-07-21 17:13:14 +0200

    Update terms wallet modal.

--- src/custom/components/WalletModal/index.tsx
+++ src/custom/components/WalletModal/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Modal from '@src/components/Modal'
 import styled from 'styled-components'
 import WalletModalMod, { WalletModalProps } from './WalletModalMod'
+import { ExternalLink } from 'theme'
+import { Trans } from '@lingui/macro'
 export * from '@src/components/WalletModal'
 
 export const GpModal = styled(Modal)`
@@ -9,6 +11,22 @@ export const GpModal = styled(Modal)`
     background-color: ${({ theme }) => theme.bg1};
   }
 `
+
+const TermsWrapper = styled.div`
+  color: ${({ theme }) => theme.text1};
+`
+
+export function CustomTerms() {
+  return (
+    <TermsWrapper>
+      <Trans>
+        By connecting a wallet, you agree to GnosisDAO&apos;s{' '}
+        <ExternalLink href="#/terms-and-conditions">Terms &amp; Conditions</ExternalLink> and acknowledge that you have
+        read, understood and agree to them.{' '}
+      </Trans>
+    </TermsWrapper>
+  )
+}
 
 export default function WalletModal(props: Omit<WalletModalProps, 'Modal'>) {
   return <WalletModalMod {...props} Modal={GpModal} />
