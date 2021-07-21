8b0b5e8d9cab7d9f2fe0cab7ac314f83de466a20
Ramiro Vazquez <ramirovazquez@gmail.com>
2021-07-21 06:11:35 -0300

    make generateReferralMetadataDoc able to receive an existing app data doc

--- src/custom/utils/metadata.ts
+++ src/custom/utils/metadata.ts
@@ -23,17 +23,24 @@ export type AppDataDoc = {
 
 export const DEFAULT_APP_CODE = 'CowSwap'
 
-export function generateReferralMetadataDoc(referralAddress: string): AppDataDoc {
-  return generateMetadataDoc({
-    referrer: {
-      kind: MetadataKind.REFERRAL,
-      referrer: referralAddress,
-      version: '1.0.0',
+export function generateReferralMetadataDoc(
+  referralAddress: string,
+  appDataDoc: AppDataDoc = generateAppDataDoc()
+): AppDataDoc {
+  return {
+    ...appDataDoc,
+    metadata: {
+      ...appDataDoc.metadata,
+      referrer: {
+        kind: MetadataKind.REFERRAL,
+        referrer: referralAddress,
+        version: '1.0.0',
+      },
     },
-  })
+  }
 }
 
-export function generateMetadataDoc(metadata: MetadataDoc = {}): AppDataDoc {
+export function generateAppDataDoc(metadata: MetadataDoc = {}): AppDataDoc {
   return {
     version: '1.0.0',
     appCode: DEFAULT_APP_CODE,
