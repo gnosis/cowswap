90f3fa06d5247840d94bddc81f46c5b154d43f7f
David W3stside <david.sato64@gmail.com>
2021-07-26 18:12:42 +0200

    move GpModal to modal mod

---
+++ src/custom/components/Modal/index.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import Modal from '@src/components/Modal'
+
+export * from '@src/components/Modal'
+export { default } from '@src/components/Modal'
+
+export const GpModal = styled(Modal)`
+  > [data-reach-dialog-content] {
+    background-color: ${({ theme }) => theme.bg1};
+
+    .bottom-close-button {
+      display: none;
+      margin: auto auto 0;
+
+      ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+      display: flex;
+    `}
+    }
+
+    ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    max-height: 100vh;
+    height: 100vh;
+    max-width: 100vw;
+    width:  100vw;
+    border-radius: 0px;
+  `}
+  }
+`
