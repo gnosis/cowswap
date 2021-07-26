3ef5399851b626c48257b8364623bb1b0fa7b0f5
Mati Dastugue <mdastugu@amazon.com>
2021-07-26 17:48:19 -0300

    Use method to handle metadata doc

--- src/custom/hooks/useMetadataDoc.ts
+++ src/custom/hooks/useMetadataDoc.ts
@@ -1,22 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useActiveWeb3React } from 'hooks/web3'
-
-interface AffiliateDoc {
-  version: string
-  appCode: string
-  metadata: any
-}
+import { generateReferralMetadataDoc, AppDataDoc } from 'utils/metadata'
 
 export function useMetadataDoc() {
   const { account } = useActiveWeb3React()
-  const [document, setDocument] = useState(<AffiliateDoc>{
-    version: '1.0.0',
-    appCode: 'CowSwap',
-    metadata: {},
-  })
+  const [document, setDocument] = useState<AppDataDoc>(generateReferralMetadataDoc(account || ''))
 
   useEffect(() => {
-    const fetchDocument = async (): Promise<AffiliateDoc | undefined> => {
+    const fetchDocument = async (): Promise<AppDataDoc | undefined> => {
       if (!account) return
       /* 
         Call API with account address. If no document, return default otherwise return metadata
