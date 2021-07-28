3dfba4b4afbcbd52900ae25f31a5bf5016dc1e3b
Leandro Boscariol <alfetopito@users.noreply.github.com>
2021-07-28 14:37:23 -0700

    Fix issue with small prices (#1091)
    * Adjusting smallLimit for fiat fee display
    
    * Converting values to string with as much dec as possible to not lose data
    
    * Replaced magic number
    
    Co-authored-by: Leandro Boscariol <leandro.boscariol@gnosis.io>

--- src/custom/components/swap/TradePrice/index.tsx
+++ src/custom/components/swap/TradePrice/index.tsx
@@ -3,7 +3,7 @@ import TradePriceMod, { TradePriceProps } from './TradePriceMod'
 import { useUSDCValue } from 'hooks/useUSDCPrice'
 import { formatSmart } from 'utils/format'
 import { tryParseAmount } from 'state/swap/hooks'
-import { FIAT_PRECISION } from 'constants/index'
+import { FIAT_PRECISION, MAX_PRECISION } from 'constants/index'
 
 export * from './TradePriceMod'
 
@@ -13,12 +13,12 @@ export default function TradePrice(props: Omit<TradePriceProps, 'fiatValue'>) {
   const priceSide = useMemo(
     () =>
       !showInverted
-        ? tryParseAmount(price.invert().toFixed(), price.baseCurrency)
-        : tryParseAmount(price.toFixed(), price.quoteCurrency),
+        ? tryParseAmount(price.invert().toFixed(MAX_PRECISION), price.baseCurrency)
+        : tryParseAmount(price.toFixed(MAX_PRECISION), price.quoteCurrency),
     [price, showInverted]
   )
   const amount = useUSDCValue(priceSide)
-  const fiatValueFormatted = formatSmart(amount, FIAT_PRECISION)
+  const fiatValueFormatted = formatSmart(amount, FIAT_PRECISION, { smallLimit: '0.01' })
 
   return <TradePriceMod {...props} fiatValue={fiatValueFormatted} />
 }
