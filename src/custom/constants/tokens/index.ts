828c81e94d2f0e9c5e2ffed07a98412407375f0d
Anxo Rodriguez <anxolin@gmail.com>
2021-07-29 19:44:16 +0200

    Fix rinkeby images

--- src/custom/constants/tokens/index.ts
+++ src/custom/constants/tokens/index.ts
@@ -1,10 +1,21 @@
+import { ChainId } from '@uniswap/sdk'
+import { WETH9 } from '@uniswap/sdk-core'
+import { DAI_RINKEBY, USDC_RINKEBY, USDT_RINKEBY, WBTC_RINKEBY } from '@src/custom/utils/rinkeby/constants'
 import { getTokenLogoURL } from 'components/CurrencyLogo'
-import { USDC, USDT, WBTC } from 'constants/tokens'
+import { DAI, USDC, USDT, WBTC } from 'constants/tokens'
 import { USDC_XDAI, USDT_XDAI, WBTC_XDAI, WXDAI } from 'utils/xdai/constants'
 
 export * from './tokensMod'
 
 export const ADDRESS_IMAGE_OVERRIDE = {
+  // Rinkeby
+  [DAI_RINKEBY.address]: getTokenLogoURL(DAI.address),
+  [USDC_RINKEBY.address]: getTokenLogoURL(USDC.address),
+  [USDT_RINKEBY.address]: getTokenLogoURL(USDT.address),
+  [WBTC_RINKEBY.address]: getTokenLogoURL(WBTC.address),
+  [WETH9[ChainId.RINKEBY].address]: getTokenLogoURL(WETH9[ChainId.MAINNET].address),
+
+  // xDai
   [USDC_XDAI.address]: getTokenLogoURL(USDC.address),
   [USDT_XDAI.address]: getTokenLogoURL(USDT.address),
   [WBTC_XDAI.address]: getTokenLogoURL(WBTC.address),
