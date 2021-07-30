9c0e3cec6720eb43a2ecb7d7e65df7967bf4e9ba
Mati Dastugue <mdastugu@amazon.com>
2021-07-30 11:19:56 -0300

    Add referral address to hook

--- src/custom/hooks/useMetadataDoc.ts
+++ src/custom/hooks/useMetadataDoc.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import { useActiveWeb3React } from 'hooks/web3'
 import { generateReferralMetadataDoc, AppDataDoc } from 'utils/metadata'
+import useParseReferralQueryParam from 'hooks/useParseReferralQueryParam'
 
 export function useMetadataDoc() {
   const { account } = useActiveWeb3React()
-  const [document, setDocument] = useState<AppDataDoc>(generateReferralMetadataDoc(account || ''))
+  const referralAddress = useParseReferralQueryParam()
+  const [document, setDocument] = useState<AppDataDoc>(generateReferralMetadataDoc(referralAddress || ''))
 
   useEffect(() => {
     if (!account) return
