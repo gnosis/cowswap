73ee29af352d1db6faff35aeb522d1b6a066500e
biocom <michel@gnosis.pm>
2021-08-02 13:56:39 +0200

    Fix manage tokens darkmode styles.

--- src/custom/components/SearchModal/ManageTokens/index.tsx
+++ src/custom/components/SearchModal/ManageTokens/index.tsx
@@ -1,15 +1,71 @@
 import React from 'react'
 import { Token } from '@uniswap/sdk-core'
-import { DefaultTheme } from 'styled-components/macro'
+import styled, { DefaultTheme } from 'styled-components/macro'
 import Card from 'components/Card'
 import ImportRow from 'components/SearchModal/ImportRow'
 import ManageTokensMod, { ManageTokensProps } from './ManageTokensMod'
 import { CurrencyModalView } from 'components/SearchModal/CurrencySearchModal'
+import { RowFixed, RowBetween } from 'components/Row'
+import { ButtonText, LinkIcon } from 'theme'
+import { SearchInput, Separator } from '@src/components/SearchModal/styleds'
+import { transparentize } from 'polished'
 
 export type ImportTokensRowProps = Omit<ManageTokensProps, 'ImportTokensRow'> & {
   theme: DefaultTheme
   searchToken: Token
 }
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: calc(100% - 60px);
+  position: relative;
+  padding-bottom: 80px;
+
+  ${SearchInput} {
+    border: 1px solid ${({ theme }) => transparentize(0.7, theme.text1)}};
+  }
+
+  ${SearchInput}:focus {
+      border-color: ${({ theme }) => theme.primary1}};
+  }
+
+  ${Separator} {
+    background: ${({ theme }) => transparentize(0.7, theme.text1)};
+  }
+
+  ${RowBetween} > div {
+    color: ${({ theme }) => theme.text1};
+  }
+
+  // Custom Tokens Title
+  ${RowBetween}:first-child > div {
+    opacity: 0.7;
+  }
+
+  // Token name
+  ${RowBetween} > div > a > div {
+    color: ${({ theme }) => theme.text1};
+  }
+
+  ${RowFixed} > svg,
+  ${RowFixed} > a {
+    transition: stroke 0.2s ease-in-out;
+  }
+
+  ${RowFixed} > svg:hover,
+  ${RowFixed} > a,
+  ${RowFixed} > a:hover > svg {
+    stroke: ${({ theme }) => theme.text1};
+  }
+
+  ${LinkIcon} {
+    stroke: ${({ theme }) => theme.text3};
+  }
+
+  ${ButtonText} > div {
+    color: ${({ theme }) => theme.primary1};
+  }
+`
 
 const ImportTokensRow = ({ theme, searchToken, setModalView, setImportToken }: ImportTokensRowProps) => (
   <Card backgroundColor={theme.bg4} padding="10px 0">
