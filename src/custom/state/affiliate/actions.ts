629e5731ce7d9267016ca73de0399d5785c13c12
Ramiro Vazquez <ramirovazquez@gmail.com>
2021-08-06 05:54:15 -0300

    add default appDataHash

--- src/custom/state/affiliate/actions.ts
+++ src/custom/state/affiliate/actions.ts
@@ -1,3 +1,4 @@
 import { createAction } from '@reduxjs/toolkit'
 
-export const updateReferralAddress = createAction<{ referralAddress: string }>('affiliate/updateReferralAddress')
+export const updateReferralAddress = createAction<string>('affiliate/updateReferralAddress')
+export const updateAppDataHash = createAction<string>('affiliate/updateAppDataHash')
