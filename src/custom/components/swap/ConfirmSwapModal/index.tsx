b06b5b06f18d0260f02994ad3917b889fe387d2a
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-08-06 13:26:07 -0700

    Added tooltip to `waiting for confirmation modal`

--- src/custom/components/swap/ConfirmSwapModal/index.tsx
+++ src/custom/components/swap/ConfirmSwapModal/index.tsx
@@ -1,4 +1,32 @@
-import ConfirmSwapModal from './ConfirmSwapModalMod'
+import React from 'react'
+import { Trans } from '@lingui/macro'
+
+import ConfirmSwapModalMod from './ConfirmSwapModalMod'
+import TradeGp from 'state/swap/TradeGp'
+import { formatSmart } from 'utils/format'
 
 export * from './ConfirmSwapModalMod'
-export default ConfirmSwapModal
+
+function buildPendingText(trade: TradeGp | undefined): JSX.Element {
+  const inputAmount = trade?.inputAmount
+  const inputSymbol = inputAmount?.currency?.symbol
+  const outputAmount = trade?.outputAmount
+  const outputSymbol = outputAmount?.currency?.symbol
+
+  return (
+    <Trans>
+      Swapping{' '}
+      <span title={`${inputAmount?.toFixed(inputAmount?.currency.decimals)} ${inputSymbol}`}>
+        {formatSmart(inputAmount) /* trade?.inputAmount?.toSignificant(6) */} {inputSymbol}
+      </span>{' '}
+      for{' '}
+      <span title={`${outputAmount?.toFixed(outputAmount?.currency.decimals)} ${outputSymbol}`}>
+        {formatSmart(outputAmount) /* trade?.outputAmount?.toSignificant(6) */} {outputSymbol}
+      </span>
+    </Trans>
+  )
+}
+
+export default function ConfirmSwapModal(props: Omit<Parameters<typeof ConfirmSwapModalMod>[0], 'buildPendingText'>) {
+  return <ConfirmSwapModalMod {...props} buildPendingText={buildPendingText} />
+}
