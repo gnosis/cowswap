651b83985bb21cb06bccb4d1d041cf93d2ab3803
Anxo Rodriguez <anxolin@gmail.com>
2021-08-10 14:11:01 +0200

    Revert "Remove dead code"
    This reverts commit 1025cc93d68daad1f48ac553aada3a9387410019.

--- src/custom/components/Markdown/index.tsx
+++ src/custom/components/Markdown/index.tsx
@@ -2,11 +2,11 @@ import React, { ReactNode } from 'react'
 import ReactMarkdownHtml from 'react-markdown/with-html'
 import ReactMarkdown, { ReactMarkdownPropsBase } from 'react-markdown'
 import useFetchFile from 'hooks/useFetchFile'
-import { HeadingRenderer, LinkScrollable, Link } from './renderers'
+import { HeadingRenderer } from './renderers'
 import Page, { Title, Content } from 'components/Page'
 import styled from 'styled-components'
 import { WithClassName } from 'types'
-import {} from 'comp'
+import { LinkScrollable, Link } from 'components/Link'
 
 interface MarkdownParams extends WithClassName {
   contentFile: string
@@ -20,7 +20,7 @@ export function Markdown(props: { children?: string }) {
   return <ReactMarkdown renderers={{ link: Link }}>{children}</ReactMarkdown>
 }
 
-const CustomMarkdownContent = (props: ReactMarkdownPropsBase & { children: string }) => (
+const MarkdownContent = (props: ReactMarkdownPropsBase & { children: string }) => (
   <ReactMarkdownHtml {...props} renderers={{ heading: HeadingRenderer, link: LinkScrollable }} allowDangerousHtml />
 )
 
@@ -30,9 +30,16 @@ export function MarkdownPage({ contentFile, title, className }: MarkdownParams) 
     <Wrapper className={className}>
       {title && <Title>{title}</Title>}
       <Content>
-        {file && <CustomMarkdownContent>{file}</CustomMarkdownContent>}
-        {error && <CustomMarkdownContent>{error}</CustomMarkdownContent>}
+        {file && <MarkdownContent>{file}</MarkdownContent>}
+        {error && <MarkdownContent>{error}</MarkdownContent>}
       </Content>
+      {/* <ScrollToTop
+        styleProps={{
+          bottom: '8.8%',
+          right: 'calc(50% - 4.6rem)',
+          background: '#9bd7c2'
+        }}
+      /> */}
     </Wrapper>
   )
 }
