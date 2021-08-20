cf3fc71ece6edb96929632df3e6598f357c8eabb
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-08-20 11:04:15 -0700

    Displaying prices with 20 decimals if needed to show everything

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -17,6 +17,7 @@ export const DEFAULT_PRECISION = 6
 export const DEFAULT_SMALL_LIMIT = '0.000001'
 export const AMOUNT_PRECISION = 4
 export const LONG_PRECISION = 10
+export const FULL_PRICE_PRECISION = 20
 export const FIAT_PRECISION = 2
 export const PERCENTAGE_PRECISION = 2
 
