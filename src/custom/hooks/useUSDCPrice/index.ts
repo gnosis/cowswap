b07b95a9e3056fa0d7c2fdfb4c990573442715d3
David W3stside <david.sato64@gmail.com>
2021-08-27 18:07:13 +0100

    coingecko usd hooks

--- src/custom/hooks/useUSDCPrice/index.ts
+++ src/custom/hooks/useUSDCPrice/index.ts
@@ -9,6 +9,12 @@ import { stringToCurrency } from 'state/swap/extension'
 import { SupportedChainId } from 'constants/chains'
 import { USDC_XDAI } from 'utils/xdai/constants'
 import { OrderKind } from 'state/orders/actions'
+import { CoinGeckoUsdPriceParams, getUSDPriceQuote, toPriceInformation } from 'api/coingecko'
+import { tryParseAmount } from 'state/swap/hooks'
+import { DEFAULT_NETWORK_FOR_LISTS } from 'constants/lists'
+import { useToken } from 'hooks/Tokens'
+import { currencyId } from 'utils/currencyId'
+import { parseUnits } from 'ethers/lib/utils'
 
 export * from '@src/hooks/useUSDCPrice'
 
@@ -83,13 +89,14 @@ export default function useUSDCPrice(currency?: Currency) {
   return { price: bestUsdPrice, error }
 }
 
-/**
- * Returns the price in USDC of the input currency from price APIs
- * @param currencyAmount currency to compute the USDC price of
- */
-export function useUSDCValue(currencyAmount?: CurrencyAmount<Currency>) {
-  const { price, error } = useUSDCPrice(currencyAmount?.currency)
+interface GetPriceQuoteParams {
+  currencyAmount?: CurrencyAmount<Currency>
+  error: Error | null
+  price: Price<Token, Currency> | null
+}
 
+// common logic for returning price quotes
+function useGetPriceQuote({ price, error, currencyAmount }: GetPriceQuoteParams) {
   return useMemo(() => {
     if (!price || error || !currencyAmount) return null
 
@@ -99,4 +106,100 @@ export function useUSDCValue(currencyAmount?: CurrencyAmount<Currency>) {
       return null
     }
   }, [currencyAmount, error, price])
+}
+
+/**
+ * Returns the price in USDC of the input currency from price APIs
+ * @param currencyAmount currency to compute the USDC price of
+ */
+export function useUSDCValue(currencyAmount?: CurrencyAmount<Currency>) {
+  const usdcPrice = useUSDCPrice(currencyAmount?.currency)
+
+  return useGetPriceQuote({ ...usdcPrice, currencyAmount })
+}
+
+export function useCoingeckoUsdPrice({ tokenAddress }: Pick<CoinGeckoUsdPriceParams, 'tokenAddress'>) {
+  // default to MAINNET (if disconnected e.g)
+  const { chainId = DEFAULT_NETWORK_FOR_LISTS } = useActiveWeb3React()
+  const [price, setPrice] = useState<Price<Token, Currency> | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+
+  const currency = useToken(tokenAddress)
+
+  useEffect(() => {
+    getUSDPriceQuote({
+      chainId,
+      tokenAddress,
+    })
+      .then(toPriceInformation)
+      .then((response) => {
+        if (!currency || !response?.amount) return
+
+        const { amount } = response
+        // api returns base units, we want atoms and CurrencyAmount
+        const usdParsed = tryParseAmount(amount.toString(), currency)
+        // parse failure is unlikely - type safe
+        if (!usdParsed) return
+
+        // create a new Price object
+        const usdPrice = new Price({
+          quoteAmount: CurrencyAmount.fromRawAmount(
+            usdParsed.currency,
+            // we use 1 as the denominator
+            parseUnits('1', usdParsed.currency.decimals).toString()
+          ),
+          baseAmount: usdParsed,
+        })
+
+        setPrice(usdPrice)
+      })
+      .catch((error) => {
+        console.error(
+          '[useUSDCPrice::useCoingeckoUsdPrice]::Error getting USD price from Coingecko for token',
+          tokenAddress,
+          error
+        )
+        return batchedUpdate(() => {
+          setError(new Error(error))
+          setPrice(null)
+        })
+      })
+  }, [chainId, currency, tokenAddress])
+
+  return { price, error }
+}
+
+type CoinGeckoUsdValueParams = Pick<CoinGeckoUsdPriceParams, 'tokenAddress'> & {
+  currencyAmount?: CurrencyAmount<Currency>
+}
+
+export function useCoingeckoUSDValue(params: CoinGeckoUsdValueParams) {
+  const { currencyAmount } = params
+  const coingeckoUsdPrice = useCoingeckoUsdPrice(params)
+
+  return useGetPriceQuote({ ...coingeckoUsdPrice, currencyAmount })
+}
+
+export function useHigherUSDValue(currencyAmount: CurrencyAmount<Currency> | undefined) {
+  const usdcValue = useUSDCValue(currencyAmount)
+  const coingeckoUsdPrice = useCoingeckoUSDValue({
+    tokenAddress: currencyAmount ? currencyId(currencyAmount.currency) : '',
+    currencyAmount,
+  })
+
+  return useMemo(() => {
+    // USDC PRICE UNAVAILABLE
+    if (!usdcValue && coingeckoUsdPrice) {
+      return coingeckoUsdPrice
+      // COINGECKO PRICE UNAVAILABLE
+    } else if (usdcValue && !coingeckoUsdPrice) {
+      return usdcValue
+      // BOTH AVAILABLE
+    } else if (usdcValue && coingeckoUsdPrice) {
+      // take the greater of the 2 values
+      return usdcValue.greaterThan(coingeckoUsdPrice) ? usdcValue : coingeckoUsdPrice
+    } else {
+      return null
+    }
+  }, [usdcValue, coingeckoUsdPrice])
 }
