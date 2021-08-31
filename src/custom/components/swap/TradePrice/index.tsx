49951f35ad9872bff3df35a248e093d5007a30e3
David W3stside <david.sato64@gmail.com>
2021-08-31 11:51:47 +0100

    re-simplify TradePrice

--- src/custom/components/swap/TradePrice/index.tsx
+++ src/custom/components/swap/TradePrice/index.tsx
@@ -1,25 +1,8 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import TradePriceMod, { TradePriceProps } from './TradePriceMod'
-import { useHigherUSDValue /*  useUSDCValue */ } from 'hooks/useUSDCPrice'
-import { formatSmart } from 'utils/format'
-import { tryParseAmount } from 'state/swap/hooks'
-import { FIAT_PRECISION } from 'constants/index'
 
 export * from './TradePriceMod'
 
-export default function TradePrice(props: Omit<TradePriceProps, 'fiatValue'>) {
-  const { price, showInverted } = props
-
-  const priceSide = useMemo(
-    () =>
-      !showInverted
-        ? tryParseAmount(price.invert().toFixed(price.baseCurrency.decimals), price.baseCurrency)
-        : tryParseAmount(price.toFixed(price.quoteCurrency.decimals), price.quoteCurrency),
-    [price, showInverted]
-  )
-  // const amount = useUSDCValue(priceSide)
-  const amount = useHigherUSDValue(priceSide)
-  const fiatValueFormatted = formatSmart(amount, FIAT_PRECISION)
-
-  return <TradePriceMod {...props} fiatValue={fiatValueFormatted} />
+export default function TradePrice(props: TradePriceProps) {
+  return <TradePriceMod {...props} />
 }
