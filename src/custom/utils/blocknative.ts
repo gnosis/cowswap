b365896ce15539fee400994f3296a19566d392c4
Ramiro Vazquez <ramirovazquez@gmail.com>
2021-09-02 05:32:44 -0300

    remove commented stuff

--- src/custom/utils/blocknative.ts
+++ src/custom/utils/blocknative.ts
@@ -1,5 +1,5 @@
 import BlocknativeSdk from 'bnc-sdk'
-import { SDKError /*, TransactionEvent*/ } from 'bnc-sdk/dist/types/src/interfaces'
+import { SDKError } from 'bnc-sdk/dist/types/src/interfaces'
 import { getSupportedChainIds } from 'connectors'
 
 export const sdk = getSupportedChainIds().reduce<Record<number, BlocknativeSdk>>((acc, networkId) => {
@@ -7,7 +7,6 @@ export const sdk = getSupportedChainIds().reduce<Record<number, BlocknativeSdk>>
     dappId: process.env.REACT_APP_BLOCKNATIVE_API_KEY || '',
     networkId,
     name: 'bnc_' + networkId,
-    //transactionHandlers: [(event: TransactionEvent) => console.log(event.transaction)],
     onerror: (error: SDKError) => {
       console.log(error)
     },
