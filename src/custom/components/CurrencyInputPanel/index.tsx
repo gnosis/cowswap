4dfcb421dedccb3fc442a71053debbbbd78c1d58
biocom <michel@gnosis.pm>
2021-09-06 12:33:17 +0200

    Remove unused type.

--- src/custom/components/CurrencyInputPanel/index.tsx
+++ src/custom/components/CurrencyInputPanel/index.tsx
@@ -101,7 +101,7 @@ export const AuxInformationContainer = styled(Container)<{
   }
 `
 
-export const Wrapper = styled.div<{ selected: boolean; showLoader: boolean; showMaxButton?: boolean }>`
+export const Wrapper = styled.div<{ selected: boolean; showLoader: boolean }>`
   // CSS Override
 
   ${InputPanel} {
