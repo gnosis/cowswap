076f1a329d02e5997275f6aa118eec15915746f9
Anxo Rodriguez <anxolin@gmail.com>
2021-10-27 14:52:54 +0100

    Remove dot

--- src/custom/components/TransactionConfirmationModal/index.tsx
+++ src/custom/components/TransactionConfirmationModal/index.tsx
@@ -373,13 +373,13 @@ function getWalletNameLabel(walletType: WalletType): string {
 function getOperationMessage(operationType: OperationType): string {
   switch (operationType) {
     case OperationType.WRAP_ETHER:
-      return 'Wrap Ether.'
+      return 'Wrap Ether'
     case OperationType.UNWRAP_WETH:
-      return 'Wrap Ether.'
+      return 'Wrap Ether'
     case OperationType.APPROVE_TOKEN:
-      return 'Approve the token.'
+      return 'Approve the token'
     case OperationType.ORDER_CANCEL:
-      return 'Soft cancel your order.'
+      return 'Soft cancel your order'
 
     default:
       return 'Almost there!'
