7a618961a41d4a8900debce39495f81ab1ee7792
biocom <michel@gnosis.pm>
2021-10-27 15:18:33 +0200

    Transaction/Orders styles update.

--- src/custom/components/AccountDetails/Transaction/styled.ts
+++ src/custom/components/AccountDetails/Transaction/styled.ts
@@ -24,6 +24,10 @@ export const TransactionWrapper = styled.div`
   ${RowFixed} {
     width: 100%;
   }
+
+  div > a {
+    color: ${({ theme }) => theme.primary1};
+  }
 `
 
 export const Wrapper = styled.div`
@@ -126,7 +130,7 @@ export const SummaryInner = styled.div`
     margin: 16px 0 0;
     width: 100%;
     display: grid;
-    grid-template-columns: fit-content(100%) fit-content(100%);
+    grid-template-columns: 1fr;
     grid-gap: 0 18px;
     justify-items: flex-start;
     align-items: flex-start;
@@ -439,13 +443,15 @@ export const TransactionInnerDetail = styled.div`
 `
 
 export const TextAlert = styled.div<{ isPending: boolean; isExpired: boolean; isCancelled: boolean }>`
-  background: ${({ isPending }) => (isPending ? 'rgb(255 87 34 / 15%)' : 'rgb(0 216 151 / 15%)')};
-  margin: 6px 0 3px;
+  background: ${({ theme, isPending }) =>
+    isPending ? transparentize(0.85, theme.attention) : transparentize(0.85, theme.success)};
+  margin: 6px 0 16px;
   padding: 8px 12px;
-  color: ${({ isPending }) => (isPending ? '#ff5722' : '#00d897')};
+  color: ${({ theme, isPending }) => (isPending ? theme.attention : theme.success)};
   text-decoration: ${({ isExpired, isCancelled }) => (isExpired || isCancelled) && 'line-through'};
   border-radius: 8px;
   text-align: center;
+  font-weight: 600;
 `
 
 export const CreationDateText = styled.div`
