fbc6713d76755fbadc87b72b896b4ce226dbe5c3
Mati Dastugue <mdastugu@amazon.com>
2021-11-02 16:43:51 -0300

    Remove profile page access

--- src/custom/components/Header/index.tsx
+++ src/custom/components/Header/index.tsx
@@ -248,7 +248,7 @@ export default function Header() {
           </Title>
           <HeaderLinks>
             <StyledNavLink to="/swap">Swap</StyledNavLink>
-            <StyledNavLink to="/profile">Profile</StyledNavLink>
+            {/*<StyledNavLink to="/profile">Profile</StyledNavLink>*/}
           </HeaderLinks>
         </HeaderRow>
         <HeaderControls>
