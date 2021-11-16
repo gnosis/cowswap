4dfc4d37d9761fe3ec5dcb49ffe4457b20ee8cfb
David W3stside <david.sato64@gmail.com>
2021-11-16 10:13:10 +0000

    remove uwc

--- src/custom/hooks/useUSDCPrice/index.ts
+++ src/custom/hooks/useUSDCPrice/index.ts
@@ -170,7 +170,6 @@ export function useCoingeckoUsdPrice(currency?: Currency) {
   const [price, setPrice] = useState<Price<Token, Currency> | null>(null)
   const [error, setError] = useState<Error | null>(null)
 
-  //uwc-debug
   useEffect(() => {
     const isSupportedChainId = supportedChainId(chainId)
     if (!isSupportedChainId || !currency) return
