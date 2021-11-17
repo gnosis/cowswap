0372888aab353c1d79e09fc91614c794ed81a370
Ramiro Vazquez <ramirovazquez@gmail.com>
2021-11-17 17:01:16 -0300

    add a new NotificationClosedUpdater to manage cross notifications close events

--- src/custom/state/affiliate/updater.tsx
+++ src/custom/state/affiliate/updater.tsx
@@ -1,11 +1,12 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 
-import { updateReferralAddress } from 'state/affiliate/actions'
+import { dismissNotification, updateReferralAddress } from 'state/affiliate/actions'
 import useParseReferralQueryParam from 'hooks/useParseReferralQueryParam'
 import { useAppDispatch } from 'state/hooks'
 import { useReferralAddress } from 'state/affiliate/hooks'
+import { AffiliateState } from './reducer'
 
-export default function ReferralLinkUpdater() {
+export function ReferralLinkUpdater() {
   const dispatch = useAppDispatch()
   const referralAddressParam = useParseReferralQueryParam()
   const referralAddress = useReferralAddress()
@@ -19,4 +20,37 @@ export default function ReferralLinkUpdater() {
   }, [referralAddressParam, referralAddress, dispatch])
 
   return null
+}
+
+export function NotificationClosedUpdater() {
+  const dispatch = useAppDispatch()
+
+  const handler = useCallback(
+    (e: StorageEvent) => {
+      if (e.key === 'redux_localstorage_simple_affiliate' && e.newValue) {
+        const parsed = JSON.parse(e.newValue) as AffiliateState
+        for (const id in parsed.isNotificationClosed) {
+          dispatch(dismissNotification(id))
+        }
+      }
+    },
+    [dispatch]
+  )
+
+  useEffect(() => {
+    window.addEventListener('storage', handler)
+
+    return () => window.removeEventListener('storage', handler)
+  }, [handler])
+
+  return null
+}
+
+export default function AffiliateUpdaters() {
+  return (
+    <>
+      <ReferralLinkUpdater />
+      <NotificationClosedUpdater />
+    </>
+  )
 }
