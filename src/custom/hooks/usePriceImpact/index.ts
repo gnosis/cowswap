12efbcd4b0e593b519939b9238ddc29142bc9dc7
David W3stside <david.sato64@gmail.com>
2021-11-25 14:09:42 +0100

    use error in index price impact

--- src/custom/hooks/usePriceImpact/index.ts
+++ src/custom/hooks/usePriceImpact/index.ts
@@ -7,8 +7,10 @@ type PriceImpactParams = Omit<FallbackPriceImpactParams, 'fiatPriceImpact'> & { 
 export default function usePriceImpact({ abTrade, parsedAmounts }: PriceImpactParams) {
   /* const fiatPriceImpact =  */ useFiatValuePriceImpact(parsedAmounts)
   // TODO: remove this - testing only - forces fallback price impact
-  const abaPriceImpact = useFallbackPriceImpact({ abTrade, fiatPriceImpact: undefined })
+  const { impact: fallbackPriceImpact, error } = useFallbackPriceImpact({ abTrade, fiatPriceImpact: undefined })
 
-  // TODO: remove this - testing only - forces ABA
-  return /* fiatPriceImpact ||  */ abaPriceImpact
+  const priceImpact = /* fiatPriceImpact ||  */ fallbackPriceImpact
+
+  // TODO: remove this - testing only - forces fallback
+  return { priceImpact, error }
 }
