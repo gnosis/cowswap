97b906c58c6bd7ed6cd9a0fe2d8b65224378b1a1
nenadV91 <nenadwebdevelop@gmail.com>
2021-11-29 23:05:29 +0100

    update wallet meta line

--- src/custom/hooks/useWalletInfo.ts
+++ src/custom/hooks/useWalletInfo.ts
@@ -65,7 +65,7 @@ function checkIsSupportedWallet(params: {
 async function getWcPeerMetadata(connector: WalletConnectConnector): Promise<{ walletName?: string; icon?: string }> {
   const provider = (await connector.getProvider()) as WalletConnectProvider
 
-  const meta = provider.signer.connection.wc.peerMeta
+  const meta = provider.walletMeta || provider.signer.connection.wc.peerMeta
   if (meta) {
     return {
       walletName: meta.name,
