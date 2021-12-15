082a88264dbed42ebaf6561958b9134e3f85db07
nenadV91 <nenadwebdevelop@gmail.com>
2021-12-15 16:52:53 +0100

    added requested PR changes

--- src/custom/api/ipfs/index.ts
+++ src/custom/api/ipfs/index.ts
@@ -6,15 +6,15 @@ type PinataPinResponse = {
   Timestamp: string
 }
 
+const pinataUrl = `${PINATA_API_URL}/pinning/pinJSONToIPFS`
+
+const headers = new Headers({
+  'Content-Type': 'application/json',
+  pinata_api_key: PINATA_API_KEY,
+  pinata_secret_api_key: PINATA_SECRET_API_KEY,
+})
+
 export async function pinJSONToIPFS(file: any): Promise<PinataPinResponse> {
-  const pinataUrl = `${PINATA_API_URL}/pinning/pinJSONToIPFS`
-
-  const headers = new Headers({
-    'Content-Type': 'application/json',
-    pinata_api_key: PINATA_API_KEY,
-    pinata_secret_api_key: PINATA_SECRET_API_KEY,
-  })
-
   const body = JSON.stringify({
     pinataContent: file,
     pinataMetadata: { name: 'appData-affiliate' },
@@ -26,17 +26,12 @@ export async function pinJSONToIPFS(file: any): Promise<PinataPinResponse> {
     body,
   })
 
-  try {
-    const response = await fetch(request)
-    const data = await response.json()
+  const response = await fetch(request)
+  const data = await response.json()
 
-    if (response.status !== 200) {
-      throw new Error(data.error.details || data.error)
-    }
-
-    return data
-  } catch (error) {
-    console.log('Failed to pin JSON to IPFS on pinata api', error)
-    return error
+  if (response.status !== 200) {
+    throw new Error(data.error.details || data.error)
   }
+
+  return data
 }
