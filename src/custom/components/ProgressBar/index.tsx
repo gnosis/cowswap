6078781597df57d077e282919a85608fa23114e3
infiniteMajor <maria.yablonskaya@altoros.com>
2021-12-23 06:45:24 +0300

    progressbar component

---
+++ src/custom/components/ProgressBar/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { ProgressBarWrap, ProgressContainer, Progress, Label, Txt, FlexWrap } from './styled'
+
+interface progressBarProps {
+  value: number
+}
+
+export function ProgressBar({ value }: progressBarProps) {
+  const [progressVal, setProgressVal] = useState(value)
+  const progressVals = [
+    {
+      value: 0,
+      label: '0%',
+    },
+    {
+      value: 25,
+      label: '25%',
+    },
+    {
+      value: 50,
+      label: '50%',
+    },
+    {
+      value: 75,
+      label: '75%',
+    },
+    {
+      value: 100,
+      label: '100%',
+    },
+  ]
+
+  return (
+    <FlexWrap>
+      <ProgressBarWrap>
+        {progressVals.map((item, index) => (
+          <Label position={item.value} onClick={() => setProgressVal(item.value)} key={`${item.value}-${index}`}>
+            {item.label}
+          </Label>
+        ))}
+        <ProgressContainer>
+          <Progress value={progressVal} />
+        </ProgressContainer>
+      </ProgressBarWrap>
+      <Txt>{progressVal}%</Txt>
+    </FlexWrap>
+  )
+}
---
+++ src/custom/components/ProgressBar/styled.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components/macro'
+import * as CSS from 'csstype'
+import { FlexWrap as FlexWrapMod } from 'pages/Profile/styled'
+import { Txt as TxtMod } from 'assets/styles/styled'
+
+export const FlexWrap = styled(FlexWrapMod)`
+  max-width: 100%;
+  align-items: flex-end;
+`
+
+export const ProgressBarWrap = styled(FlexWrapMod)`
+  max-width: 500px; //optional
+  padding-top: 40px;
+  position: relative;
+`
+
+export const ProgressContainer = styled.div`
+  background-color: transparent;
+  height: 30px;
+  width: 100% !important;
+  border: 1px solid ${({ theme }) => theme.cardBorder};
+  position: relative;
+  overflow: hidden;
+`
+
+export const Progress = styled.div<Partial<CSS.Properties & { value: number }>>`
+  background-image: linear-gradient(to right, #d3e501, #d29145, #ce01b9);
+  width: 100%;
+  max-width: ${(props) => props.value}%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  transition: max-width 0.2s;
+`
+
+export const Label = styled.a<Partial<CSS.Properties & { position: any }>>`
+  cursor: pointer;
+  position: absolute;
+  top: 10px;
+  left: ${(props) => props.position}%;
+  transform: translateX(-50%);
+  font-weight: bold;
+  text-decoration: underline;
+
+  &:hover {
+    text-decoration: none;
+  }
+`
+
+export const Txt = styled(TxtMod)`
+  font-size: 20px;
+  font-weight: bold;
+  margin-left: 15px;
+`
