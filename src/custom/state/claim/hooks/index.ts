acb0d50f6f19a9cf88936c0115c2e1602c8ee934
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-12-24 08:19:23 -0300

    Added custom useClaimCallback

--- src/custom/state/claim/hooks/index.ts
+++ src/custom/state/claim/hooks/index.ts
@@ -1,12 +1,19 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import JSBI from 'jsbi'
 import { CurrencyAmount, Token } from '@uniswap/sdk-core'
+import { TransactionResponse } from '@ethersproject/providers'
+
+import { useVCowContract } from 'hooks/useContract'
+import { useActiveWeb3React } from 'hooks/web3'
+import { useSingleContractMultipleData } from 'state/multicall/hooks'
+import { useTransactionAdder } from 'state/enhancedTransactions/hooks'
+
+import { V_COW } from 'constants/tokens'
+
+import { formatSmart } from 'utils/format'
+import { calculateGasMargin } from 'utils/calculateGasMargin'
 
 import { useUserClaims } from 'state/claim/hooks/hooksMod'
-import { useVCowContract } from 'hooks/useContract'
-import { useSingleContractMultipleData } from 'state/multicall/hooks'
-import { useActiveWeb3React } from 'hooks/web3'
-import { V_COW } from 'constants/tokens'
 
 export * from './hooksMod'
 
@@ -27,6 +34,18 @@ export interface UserClaimData {
   amount: string
   proof: string[]
   type: ClaimType
+}
+
+export interface ClaimInput {
+  /**
+   * The index of the claim
+   */
+  index: number
+  /**
+   * The amount of the claim. Optional
+   * If not present, will claim the full amount
+   */
+  amount?: string
 }
 
 type Account = string | null | undefined
@@ -94,4 +113,161 @@ export function useUserUnclaimedAmount(account: string | null | undefined): Curr
   }, JSBI.BigInt('0'))
 
   return CurrencyAmount.fromRawAmount(vCow, JSBI.BigInt(totalAmount))
+}
+
+/**
+ * Hook that returns the claimCallback
+ *
+ * Different from the original version, the returned callback takes as input a list of ClaimInputs,
+ * which is an object of the claim index and the amount being claimed.
+ *
+ * @param account
+ */
+export function useClaimCallback(account: string | null | undefined): {
+  claimCallback: (claimInputs: ClaimInput[]) => Promise<string | undefined>
+} {
+  // get claim data for given account
+  const { chainId, account: connectedAccount } = useActiveWeb3React()
+  const claims = useUserAvailableClaims(account)
+  const vCowContract = useVCowContract()
+
+  // used for popup summary
+  const addTransaction = useTransactionAdder()
+  const vCowToken = chainId ? V_COW[chainId] : undefined
+
+  const claimCallback = useCallback(
+    async function (claimInput: ClaimInput[]) {
+      if (
+        claims.length === 0 ||
+        claimInput.length === 0 ||
+        !account ||
+        !connectedAccount ||
+        !chainId ||
+        !vCowContract ||
+        !vCowToken
+      ) {
+        throw new Error("Not initialized, can't claim")
+      }
+
+      const { args, totalClaimedAmount, value } = _getClaimManyArgs({ claimInput, claims, account, connectedAccount })
+
+      if (!args) {
+        throw new Error('There were no valid claims selected')
+      }
+
+      const vCowAmount = CurrencyAmount.fromRawAmount(vCowToken, totalClaimedAmount)
+
+      return vCowContract.estimateGas['claimMany'](...args, { value }).then((estimatedGas) =>
+        vCowContract
+          .claimMany(...args, {
+            from: connectedAccount,
+            gasLimit: calculateGasMargin(chainId, estimatedGas),
+            value,
+          })
+          .then((response: TransactionResponse) => {
+            addTransaction({
+              hash: response.hash,
+              summary: `Claimed ${formatSmart(vCowAmount)} vCOW`,
+              claim: { recipient: account },
+            })
+            return response.hash
+          })
+      )
+    },
+    [account, addTransaction, chainId, claims, connectedAccount, vCowContract, vCowToken]
+  )
+
+  return { claimCallback }
+}
+
+type GetClaimManyArgsParams = {
+  claimInput: ClaimInput[]
+  claims: UserClaims
+  account: string
+  connectedAccount: string
+}
+
+function _getClaimManyArgs({ claimInput, claims, account, connectedAccount }: GetClaimManyArgsParams): {
+  args: [number[], ClaimType[], string[], string[], string[], string[][], string[]] | undefined
+  totalClaimedAmount: JSBI
+  value: string | undefined
+} {
+  const indices: number[] = []
+  const claimTypes: ClaimType[] = []
+  const claimants: string[] = []
+  const claimableAmounts: string[] = []
+  const claimedAmounts: string[] = []
+  const merkleProofs: string[][] = []
+  const sendEth: string[] = []
+
+  let totalClaimedAmount = JSBI.BigInt('0')
+  let totalValue = JSBI.BigInt('0')
+
+  // Creating a map for faster access when checking what's being claimed
+  const claimsMap = claims.reduce<Record<number, UserClaimData>>((acc, claim) => {
+    acc[claim.index] = claim
+    return acc
+  }, {})
+
+  claimInput.forEach((input) => {
+    const claim = claimsMap[input.index]
+
+    // It can be that the index being passed is already claimed or belongs to another account
+    // Thus, it's possible that the returned `args` is "empty"
+    if (claim) {
+      indices.push(claim.index)
+      // always the same
+      claimants.push(account)
+      // always the max available
+      claimableAmounts.push(claim.amount)
+
+      claimTypes.push(claim.type)
+      // depends on claim type and whether claimed account == connected account
+      const claimedAmount = _getClaimedAmount({ claim, input, account, connectedAccount })
+      claimedAmounts.push(claimedAmount)
+
+      merkleProofs.push(claim.proof)
+      // only used on UserOption, equal to claimedAmount
+      const value = claim.type === ClaimType.UserOption ? claimedAmount : '0'
+      sendEth.push(value) // TODO: verify ETH balance < input.amount ?
+
+      // sum of claimedAmounts for the toast notification
+      totalClaimedAmount = JSBI.add(totalClaimedAmount, JSBI.BigInt(claimedAmount))
+      // sum of Native currency to be used on call options
+      totalValue = JSBI.add(totalValue, JSBI.BigInt(value))
+    }
+  })
+
+  return {
+    args:
+      indices.length > 0
+        ? [indices, claimTypes, claimants, claimableAmounts, claimedAmounts, merkleProofs, sendEth]
+        : undefined,
+    totalClaimedAmount,
+    value: totalValue.toString() === '0' ? undefined : totalValue.toString(),
+  }
+}
+
+type GetClaimedAmountParams = Pick<GetClaimManyArgsParams, 'account' | 'connectedAccount'> & {
+  claim: UserClaimData
+  input: ClaimInput
+}
+
+function _getClaimedAmount({ claim, input, account, connectedAccount }: GetClaimedAmountParams): string {
+  if (
+    // claiming a paid claim on behalf of someone else?
+    (account !== connectedAccount && claim.type in PAID_CLAIM_TYPES) ||
+    // claiming a free claim?
+    claim.type in FREE_CLAIM_TYPES ||
+    // did not pass in input.amount?
+    !input.amount ||
+    // trying to send more than what's available?
+    JSBI.greaterThan(JSBI.BigInt(input.amount), JSBI.BigInt(claim.amount))
+  ) {
+    // use full amount
+    return claim.amount
+  } else {
+    // use partial amount
+    return input.amount
+  }
 }
