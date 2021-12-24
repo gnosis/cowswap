0c760b8b8c48e0bbd7060e12bd75fafcf9995ed7
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-12-24 16:59:03 -0300

    Updating fake data and commenting out real implementation

--- src/components/claim/ClaimModal.tsx
+++ src/components/claim/ClaimModal.tsx
@@ -53,7 +53,7 @@ export default function ClaimModal() {
 
   const { chainId } = useActiveWeb3React()
 
-  const account = '0x00059034c8759f5C89af6792Eb3cFa2CCd12f6f3'
+  const account = '0x0010B775429d6C92333E363CBd6BF28dDF1A87E6'
   // used for UI loading states
   const [attempting, setAttempting] = useState<boolean>(false)
 
@@ -69,7 +69,7 @@ export default function ClaimModal() {
   function onClaim() {
     console.log(`Trying to claim!!!`, unclaimedAmount?.toString(), claimConfirmed)
     setAttempting(true)
-    claimCallback([{ index: 2 }])
+    claimCallback([{ index: 3 }])
       // reset modal and log error
       .catch((error) => {
         setAttempting(false)
