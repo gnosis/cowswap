553a21ce991783abb245a2b0c9b60241789d0e35
Leandro <leandro.boscariol@gnosis.io>
2022-01-07 14:48:42 -0800

    Show '0' when formatted amount is falsy

--- src/custom/pages/Profile/index.tsx
+++ src/custom/pages/Profile/index.tsx
@@ -75,7 +75,7 @@ export default function Profile() {
               <ProfileFlexCol>
                 <Txt fs={14}>Balance</Txt>
                 <Txt fs={18} title={`${formatMax(vCowBalance)} vCOW`}>
-                  <strong>{formatSmart(vCowBalance, AMOUNT_PRECISION)} vCOW</strong>
+                  <strong>{formatSmart(vCowBalance, AMOUNT_PRECISION) || '0'} vCOW</strong>
                 </Txt>
               </ProfileFlexCol>
             </VCOWBalance>
