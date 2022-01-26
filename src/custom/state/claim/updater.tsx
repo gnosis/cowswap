245ad353bb6169344a35c8901a9ab3d660dd22f1
David W3stside <david.sato64@gmail.com>
2022-01-26 14:31:01 -0300

    update chains

--- src/custom/state/claim/updater.tsx
+++ src/custom/state/claim/updater.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
 import { useBlockNumber } from '../application/hooks'
-import { ALL_SUPPORTED_CHAIN_IDS } from 'constants/chains'
+import { SupportedChainId } from 'constants/chains'
 import { supportedChainId } from 'utils/supportedChainId'
 import { useActiveWeb3React } from 'hooks/web3'
 import { fetchClaims, useClaimDispatchers } from './hooks'
+
+const SUPPORTED_CLAIM_CHAINS = [SupportedChainId.MAINNET, SupportedChainId.XDAI]
 
 export default function Updater() {
   const { account, chainId } = useActiveWeb3React()
@@ -14,7 +16,7 @@ export default function Updater() {
     if (!account) return
 
     const supportedChain = supportedChainId(chainId)
-    ALL_SUPPORTED_CHAIN_IDS.forEach((chain) => {
+    SUPPORTED_CLAIM_CHAINS.forEach((chain) => {
       if (supportedChain === chain) return
       // Chain at this point is a supported chain
       fetchClaims(account, chain)
