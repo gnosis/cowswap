23f4e44f681eebf3eb9bfa4b8fe9562d72fe1b93
David W3stside <david.sato64@gmail.com>
2022-01-26 16:53:56 -0300

    saving

--- src/custom/hooks/useChangeNetworks.ts
+++ src/custom/hooks/useChangeNetworks.ts
@@ -2,21 +2,31 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core'
 import { useOnClickOutside } from 'hooks/useOnClickOutside'
 import { useActiveWeb3React } from 'hooks/web3'
-import { useModalOpen, useToggleModal, useWalletModalToggle } from 'state/application/hooks'
+import {
+  useCloseModals,
+  useModalOpen,
+  useOpenModal,
+  // useToggleModal,
+  useWalletModalToggle,
+} from 'state/application/hooks'
 import { ApplicationModal } from 'state/application/reducer'
 import { useAppSelector } from 'state/hooks'
 import { CHAIN_INFO, SupportedChainId } from 'constants/chains'
 import { switchToNetwork } from 'utils/switchToNetwork'
 import { supportedChainId } from 'utils/supportedChainId'
 
-export default function useChangeNetworks() {
-  const { chainId: preChainId, library, account } = useActiveWeb3React()
+type ChangeNetworksParams = Pick<ReturnType<typeof useActiveWeb3React>, 'account' | 'chainId' | 'library'>
+
+export default function useChangeNetworks({ account, chainId: preChainId, library }: ChangeNetworksParams) {
   const { error } = useWeb3React() // MOD: check unsupported network
-  const node = useRef<HTMLDivElement>()
-  const open = useModalOpen(ApplicationModal.NETWORK_SELECTOR)
-  const toggle = useToggleModal(ApplicationModal.NETWORK_SELECTOR)
+  const nodeRef = useRef<HTMLDivElement>()
+  const isModalOpen = useModalOpen(ApplicationModal.NETWORK_SELECTOR)
+  const closeModal = useCloseModals()
+  const openModal = useOpenModal(ApplicationModal.NETWORK_SELECTOR)
   const toggleWalletModal = useWalletModalToggle() // MOD
-  useOnClickOutside(node, open ? toggle : undefined)
+
+  useOnClickOutside(nodeRef, isModalOpen ? closeModal : undefined)
+
   const implements3085 = useAppSelector((state) => state.application.implements3085)
 
   // MOD: get supported chain and check unsupported
@@ -33,28 +43,37 @@ export default function useChangeNetworks() {
 
   const conditionalToggle = useCallback(() => {
     if (showSelector) {
-      toggle()
+      if (isModalOpen) {
+        alert('called')
+        closeModal()
+      } else {
+        alert('called close')
+        openModal()
+      }
     }
-  }, [showSelector, toggle])
+  }, [closeModal, isModalOpen, openModal, showSelector])
 
   // MOD: checks if a requested network switch was sent
   // used for when user disconnected and selects a network internally
   // if 3085 supported, will connect wallet and change network
   const [queuedNetworkSwitch, setQueuedNetworkSwitch] = useState<null | number>(null)
 
+  // uwc-debug
   const networkCallback = useCallback(
     (supportedChainId) => {
+      console.debug('adasdasdasdasdasd')
       if (!account) {
         toggleWalletModal()
         return setQueuedNetworkSwitch(supportedChainId)
       } else if (implements3085 && library && supportedChainId) {
         switchToNetwork({ library, chainId: supportedChainId })
-        return open && toggle()
+
+        return isModalOpen && closeModal()
       }
 
       return
     },
-    [account, implements3085, library, open, toggle, toggleWalletModal]
+    [account, implements3085, library, toggleWalletModal, isModalOpen, closeModal]
   )
 
   // if wallet supports 3085
@@ -68,8 +87,13 @@ export default function useChangeNetworks() {
   return {
     callback: networkCallback,
     conditionalToggle,
+    openModal,
+    closeModal,
+    isModalOpen,
     isUnsupportedChain,
     chainInfo: info,
     mainnetInfo,
+    showSelector,
+    nodeRef,
   }
 }
