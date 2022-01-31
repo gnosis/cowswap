5d5dc5c47a09f1f7e1803dec4ac74f48c8b09b90
Leandro <leandro.boscariol@gnosis.io>
2022-01-31 13:59:30 -0800

    Disable claiming while isCi is set

--- src/custom/pages/Claim/const.ts
+++ src/custom/pages/Claim/const.ts
@@ -1,4 +1,4 @@
-import { isProd, isEns, isBarn } from 'utils/environments'
+import { isProd, isEns, isBarn, isCi } from 'utils/environments'
 
-export const IS_CLAIMING_ENABLED = !isProd && !isEns && !isBarn
+export const IS_CLAIMING_ENABLED = !isProd && !isEns && !isBarn && !isCi
 export const IS_TESTING_ENV = !isProd && !isEns && !isBarn
