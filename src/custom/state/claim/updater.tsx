76403160bfb08f120a0195e18d234d12a858de5e
Leandro <leandro.boscariol@gnosis.io>
2022-02-04 15:54:21 -0800

    Updated the updater to store the count of classified claims

--- src/custom/state/claim/updater.tsx
+++ src/custom/state/claim/updater.tsx
@@ -1,24 +1,61 @@
 import { useEffect } from 'react'
 import { SupportedChainId } from 'constants/chains'
-import { useClaimDispatchers, useClaimState, useUserAvailableClaims } from './hooks'
+import { ClassifiedUserClaims, useClaimDispatchers, useClaimState, useClassifiedUserClaims } from './hooks'
+import { useActiveWeb3React } from 'hooks'
+import { ChainClaimsCount } from 'state/claim/reducer'
 
 export default function Updater() {
+  const { chainId } = useActiveWeb3React()
   const { activeClaimAccount } = useClaimState()
-  const { setHasClaimsOnOtherChains } = useClaimDispatchers()
+  const { setClaimsCount } = useClaimDispatchers()
 
-  const { claims: mainnetAvailable } = useUserAvailableClaims(activeClaimAccount, SupportedChainId.MAINNET)
-  const { claims: gnosisAvailable } = useUserAvailableClaims(activeClaimAccount, SupportedChainId.XDAI)
+  // Fetches all classified claims for mainnet and gchain
+  const mainnetClaims = useClassifiedUserClaims(activeClaimAccount, SupportedChainId.MAINNET)
+  const gChainClaims = useClassifiedUserClaims(activeClaimAccount, SupportedChainId.XDAI)
 
   useEffect(() => {
-    setHasClaimsOnOtherChains({
+    // Counts the claims, based on which is the current network
+    const mainnetCount = _countClaims(mainnetClaims, chainId, SupportedChainId.MAINNET)
+    const gChainCount = _countClaims(gChainClaims, chainId, SupportedChainId.XDAI)
+
+    // Stores it on redux
+    setClaimsCount({
       chain: SupportedChainId.MAINNET,
-      hasClaims: Boolean(mainnetAvailable && mainnetAvailable.length > 0),
+      claimsCount: mainnetCount,
     })
-    setHasClaimsOnOtherChains({
+    setClaimsCount({
       chain: SupportedChainId.XDAI,
-      hasClaims: Boolean(gnosisAvailable && gnosisAvailable.length > 0),
+      claimsCount: gChainCount,
     })
-  }, [setHasClaimsOnOtherChains, mainnetAvailable, gnosisAvailable])
+  }, [setClaimsCount, chainId, mainnetClaims, gChainClaims])
 
   return null
+}
+
+/**
+ * Counts claims per network
+ */
+function _countClaims(
+  mainnetClaims: ClassifiedUserClaims,
+  currentChainId: number | undefined,
+  chainId: SupportedChainId
+) {
+  const { available, claimed, expired } = mainnetClaims
+
+  const count: Partial<ChainClaimsCount> = {
+    // Total is easy, just add everything up
+    // This will always be accurate.
+    // When we only have airdrop repo as reference, everything will be `available`
+    // Otherwise the sum of all categories
+    total: claimed.length + available.length + expired.length,
+  }
+  // Only if we are in the network we want to check we are able to do smart contract calls
+  // and fetch contract data
+  // Otherwise the count would be replaced with outdated info
+  if (currentChainId === chainId) {
+    count.available = available.length
+    count.expired = expired.length
+    count.claimed = claimed.length
+  }
+  return count
 }
