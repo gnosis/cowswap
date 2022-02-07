88f9fd2d3f60a5f612f9c0f1daa7e9233826e8a2
Anxo Rodriguez <anxolin@gmail.com>
2022-02-07 13:07:07 +0000

    Test deployment rinkeby

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -53,7 +53,7 @@ export const GP_VAULT_RELAYER: Partial<Record<number, string>> = {
 export const V_COW_CONTRACT_ADDRESS: Record<number, string> = {
   [ChainId.MAINNET]: '0x6d04B3ad33594978D0D4B01CdB7c3bA4a90a7DFe',
   [ChainId.XDAI]: '0xA3A674a40709A837A5E742C2866eda7d3b35a7c0',
-  [ChainId.RINKEBY]: '0xD7Dd9397Fb942565959c77f8e112ec5aa7D8C92c',
+  [ChainId.RINKEBY]: '0x7cA021edDE27cC5DE7a818244aBD1d3e3D6ddE3E',
 }
 
 // See https://github.com/gnosis/gp-v2-contracts/commit/821b5a8da213297b0f7f1d8b17c893c5627020af#diff-12bbbe13cd5cf42d639e34a39d8795021ba40d3ee1e1a8282df652eb161a11d6R13
