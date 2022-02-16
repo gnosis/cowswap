b7e4cf3ee9df342b2d31757e030c4eda935dca4b
Henry Palacios <henrydpalacios@gmail.com>
2022-02-16 09:52:28 -0300

    set api key according to environment (#2422)

--- src/custom/connectors/index.ts
+++ src/custom/connectors/index.ts
@@ -4,7 +4,7 @@ import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 import { PortisConnector } from '@web3-react/portis-connector'
 
-import { FortmaticConnector } from 'connectors/Fortmatic'
+import { FortmaticConnector, getFortmaticApiKey } from 'custom/connectors/Fortmatic'
 import { NetworkConnector } from 'connectors/NetworkConnector'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
@@ -91,7 +91,7 @@ export const walletconnect = new WalletConnectConnector({
 
 // mainnet only
 export const fortmatic = new FortmaticConnector({
-  apiKey: process.env.REACT_APP_FORTMATIC_KEY ?? '',
+  apiKey: getFortmaticApiKey() ?? '',
   chainId: NETWORK_CHAIN_ID,
 })
 
