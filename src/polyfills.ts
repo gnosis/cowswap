4da0b1e7fc160a0d3726e43f2501cb0708597952
w3stside <david.sato64@gmail.com>
2022-03-11 11:02:01 +0000

    top level console override

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -5,3 +5,37 @@ import flatMap from 'array.prototype.flatmap'
 
 flat.shim()
 flatMap.shim()
+
+// define a new console
+const console = (function (originalConsole) {
+  return {
+    log(...args: any[]) {
+      if (process.env.NODE_ENV !== 'production') {
+        originalConsole.log(...args)
+      }
+    },
+    debug(...args: any[]) {
+      if (process.env.NODE_ENV !== 'production') {
+        originalConsole.debug(...args)
+      }
+    },
+    info(...args: any[]) {
+      if (process.env.NODE_ENV !== 'production') {
+        originalConsole.info(...args)
+      }
+    },
+    warn(...args: any[]) {
+      if (process.env.NODE_ENV !== 'production') {
+        originalConsole.warn(...args)
+      }
+    },
+    error(...args: any[]) {
+      if (process.env.NODE_ENV !== 'production') {
+        originalConsole.error(...args)
+      }
+    },
+  }
+})(window.console)
+
+//Then redefine the old console
+window.console = { ...window.console, ...console }
