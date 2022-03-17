7eca828ece6186c5dd49bb20adb613e5f6a5bc41
w3stside <david.sato64@gmail.com>
2022-03-17 19:02:53 +0000

    add selected tier styling to table

--- src/custom/components/CowSubsidyModal/SubsidyTable.tsx
+++ src/custom/components/CowSubsidyModal/SubsidyTable.tsx
@@ -3,12 +3,48 @@ import { formatSmartLocaleAware } from 'utils/format'
 import { ClaimTr } from 'pages/Claim/ClaimsTable'
 import { BigNumber } from 'bignumber.js'
 import { COW_SUBSIDY_DATA } from './constants'
+import { CowBalanceProps } from '../CowBalance'
+import { transparentize } from 'polished'
+import { FlyoutRowActiveIndicator } from '../Header/NetworkSelector'
 
 const StyledSubsidyTable = styled.table`
   width: 100%;
 `
 
-const SubsidyTr = styled(ClaimTr)`
+const SubsidyTr = styled(ClaimTr)<{ selected?: boolean }>`
+  position: relative;
+  ${({ selected, theme }) =>
+    selected &&
+    `
+    background: ${transparentize(0.7, theme.primary1)};
+
+    
+    > td {
+      border: 1.2px solid ${theme.primary1};
+      color: ${theme.primary1};
+      font-weight: 500;
+
+      > ${FlyoutRowActiveIndicator} {
+        position: absolute;
+        left: 8%;
+
+        background-color: ${theme.primary1};
+        box-shadow: 0px 0px 8px ${transparentize(0.3, theme.primary1)};
+      }
+
+      &:first-child {
+        border-right: none;
+
+        display: flex;
+        align-items: center; 
+        justify-content: center;
+      }
+      &:last-child {
+        border-left: none;
+      }
+    }
+  `}
+
   > th {
     font-weight: 300;
   }
@@ -21,7 +57,7 @@ const SubsidyTr = styled(ClaimTr)`
 
 const TABLE_HEADERS = ['(v)COW balance', 'Fee discount']
 
-function SubsidyTable() {
+function SubsidyTable({ subsidy }: Pick<CowBalanceProps, 'subsidy'>) {
   return (
     <StyledSubsidyTable>
       <thead>
@@ -32,13 +68,19 @@ function SubsidyTable() {
         </SubsidyTr>
       </thead>
       <tbody>
-        {COW_SUBSIDY_DATA.map(([threshold, discount], i) => (
-          <SubsidyTr key={discount + '_' + i}>
-            {/* if index != 0, show prefix '>' */}
-            <td>{i && '>' + formatSmartLocaleAware(new BigNumber(threshold))}</td>
-            <td>{discount}%</td>
-          </SubsidyTr>
-        ))}
+        {COW_SUBSIDY_DATA.map(([threshold, discount], i) => {
+          const selected = subsidy.tier === i
+          return (
+            <SubsidyTr key={discount + '_' + i} selected={selected}>
+              {/* if index != 0, show prefix '>' */}
+              <td>
+                {selected && <FlyoutRowActiveIndicator active />}
+                <span>{i && '>' + formatSmartLocaleAware(new BigNumber(threshold))}</span>
+              </td>
+              <td>{discount}%</td>
+            </SubsidyTr>
+          )
+        })}
       </tbody>
     </StyledSubsidyTable>
   )
