d10579b7d8703e0fcad5695a7ceac7cacc9889a5
w3stside <david.sato64@gmail.com>
2022-03-17 19:03:03 +0000

    change return of util

--- src/custom/components/CowSubsidyModal/utils.ts
+++ src/custom/components/CowSubsidyModal/utils.ts
@@ -5,13 +5,15 @@ export function getDiscountFromBalance(balance: BigNumber) {
   const [, firstDiscount] = COW_SUBSIDY_DATA[0]
 
   let eligibleDiscount = firstDiscount
-  for (const [threshold, discount] of COW_SUBSIDY_DATA) {
+  let tier = 0
+  for (const [i, [threshold, discount]] of COW_SUBSIDY_DATA.entries()) {
     // is balance < current threshold?
     // e.g 2443 < TIER 3: >1000? FALSE
     if (balance.lt(new BigNumber(threshold))) break
 
     eligibleDiscount = discount
+    tier = i
   }
 
-  return eligibleDiscount
+  return { discount: eligibleDiscount, tier }
 }
