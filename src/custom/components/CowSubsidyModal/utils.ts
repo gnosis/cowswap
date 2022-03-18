71909c79683f36d7a8aec29233d2e79f406a5eb3
w3stside <david.sato64@gmail.com>
2022-03-18 11:45:37 +0000

    lt inclusive in loop

--- src/custom/components/CowSubsidyModal/utils.ts
+++ src/custom/components/CowSubsidyModal/utils.ts
@@ -1,26 +1,23 @@
 import { BigNumber } from 'bignumber.js'
 import { COW_SUBSIDY_DATA } from './constants'
 
-const LAST_INDEX = COW_SUBSIDY_DATA.length - 1
-const [, firstDiscount] = COW_SUBSIDY_DATA[0]
+const SLICED_DATA = COW_SUBSIDY_DATA.slice(1)
 
 export function getDiscountFromBalance(balanceAtomsBn: BigNumber) {
-  let discount = firstDiscount
   let tier = 0
+  let [, discount] = COW_SUBSIDY_DATA[tier]
   // Here we use a sliced verison of our data without index 0 (0 amt tier)
   // because loop-wise a balance less than or equal to 0 and 100 (indices 0 and 1, respectively) are the same
-  for (const [threshold, thresholdDiscount] of COW_SUBSIDY_DATA.slice(1)) {
+  for (const [threshold, thresholdDiscount] of SLICED_DATA) {
     // Increase our tier number only if we're not at the end of our list
-    if (tier < LAST_INDEX) {
-      // Is balance less than or equal to threshold?
-      // break. it's our tier.
-      const thresholdBn = new BigNumber(threshold)
-      if (balanceAtomsBn.lte(thresholdBn)) break
+    // Is balance less than or equal to threshold?
+    // return our subsidy information
+    const thresholdBn = new BigNumber(threshold)
+    if (balanceAtomsBn.lt(thresholdBn)) return { discount, tier }
 
-      // Else assign the current discount as the threshold and iterate one tier
-      discount = thresholdDiscount
-      tier++
-    }
+    // Else assign the current discount as the threshold and iterate one tier
+    discount = thresholdDiscount
+    tier++
   }
 
   return { discount, tier }
