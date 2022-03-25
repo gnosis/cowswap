122aefab58430e98c3da3a5f7e1641b8c4a563de
w3stside <david.sato64@gmail.com>
2022-03-25 12:09:57 +0000

    show fee discount in tooltip

--- src/custom/components/swap/FeeInformationTooltip.tsx
+++ src/custom/components/swap/FeeInformationTooltip.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import TradeGp from 'state/swap/TradeGp'
 import QuestionHelper from 'components/QuestionHelper'
 import styled from 'styled-components/macro'
@@ -6,6 +6,9 @@ import { formatMax, formatSmart } from 'utils/format'
 import useTheme from 'hooks/useTheme'
 import { FIAT_PRECISION } from 'constants/index'
 import { CurrencyAmount, Token } from '@uniswap/sdk-core'
+import useCowBalanceAndSubsidy from 'hooks/useCowBalanceAndSubsidy'
+import { BigNumber } from 'bignumber.js'
+import { ONE_BIG_NUMBER } from '@gnosis.pm/dex-js'
 
 interface FeeInformationTooltipProps {
   trade?: TradeGp
@@ -31,13 +34,15 @@ export const FeeInformationTooltipWrapper = styled.div`
   height: 60px;
 `
 
-const FeeTooltipLine = styled.p`
+const FeeTooltipLine = styled.p<{ discount?: boolean }>`
   display: flex;
   flex-flow: row nowrap;
   justify-content: space-between;
   align-items: center;
   margin: 0;
   gap: 0 8px;
+
+  ${({ discount = false }) => discount && 'text-decoration: line-through;'}
 
   font-size: small;
 
@@ -75,13 +80,68 @@ const FeeInnerWrapper = styled.div`
   gap: 2px;
 `
 
+const FeeBreakdownLine = ({
+  feeAmount,
+  discount,
+  type,
+  symbol,
+}: FeeInformationTooltipProps & { symbol: string | undefined; discount: number }) => {
+  const typeString = type === 'From' ? '+' : '-'
+
+  const FeeAmount = useCallback(() => {
+    // 1 + DISCOUNT/100 e.g 1 + 0.15 = 1.15
+    const discountBn = ONE_BIG_NUMBER.plus(new BigNumber(discount).div(new BigNumber('100')))
+    // we need the fee BEFORE the discount as the backend will return us the adjusted fee with discount
+    const adjustedFee = feeAmount ? new BigNumber(feeAmount).times(discountBn).toString(10) : undefined
+
+    return (
+      <>
+        <span>Fee</span>
+        {adjustedFee ? (
+          <span>
+            {typeString}
+            {adjustedFee} {symbol}
+          </span>
+        ) : (
+          <strong className="green">Free</strong>
+        )}
+      </>
+    )
+  }, [discount, feeAmount, symbol, typeString])
+
+  const FeeDiscountedAmount = useCallback(
+    () => (
+      <>
+        <strong className="green">Fee [-{discount}%]</strong>
+        <span>
+          {typeString}
+          {feeAmount} {symbol}
+        </span>
+      </>
+    ),
+    [discount, feeAmount, symbol, typeString]
+  )
+
+  return (
+    <>
+      <FeeTooltipLine discount={!!discount}>
+        <FeeAmount />
+      </FeeTooltipLine>
+      {!!discount && (
+        <FeeTooltipLine>
+          <FeeDiscountedAmount />
+        </FeeTooltipLine>
+      )}
+    </>
+  )
+}
+
 export default function FeeInformationTooltip(props: FeeInformationTooltipProps) {
   const {
     trade,
     label,
     amountBeforeFees,
     amountAfterFees,
-    feeAmount,
     type,
     showHelper,
     fiatValue,
@@ -90,6 +150,8 @@ export default function FeeInformationTooltip(props: FeeInformationTooltipProps)
   } = props
 
   const theme = useTheme()
+
+  const { subsidy } = useCowBalanceAndSubsidy()
 
   const [symbol, fullFeeAmount] = useMemo(() => {
     const amount = trade?.[type === 'From' ? 'inputAmount' : 'outputAmount']
@@ -113,17 +175,7 @@ export default function FeeInformationTooltip(props: FeeInformationTooltipProps)
                   {amountBeforeFees} {symbol}
                 </span>{' '}
               </FeeTooltipLine>
-              <FeeTooltipLine>
-                <span>Fee</span>
-                {feeAmount ? (
-                  <span>
-                    {type === 'From' ? '+' : '-'}
-                    {feeAmount} {symbol}
-                  </span>
-                ) : (
-                  <strong className="green">Free</strong>
-                )}{' '}
-              </FeeTooltipLine>
+              <FeeBreakdownLine {...props} discount={subsidy.discount} symbol={symbol} />
               {/* TODO: Add gas costs when available (wait for design) */}
               {allowsOffchainSigning && (
                 <FeeTooltipLine>
